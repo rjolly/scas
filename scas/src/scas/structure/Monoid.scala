7bff7545c1bf486f4b0400c630f8a85791075bf6
Raphael Jolly <rjolly@users.sourceforge.net>
2020-02-16 12:46:20 +0100

    empty

--- scas/src/scas/structure/Monoid.scala
+++ scas/src/scas/structure/Monoid.scala
@@ -1,10 +1,16 @@
 package scas.structure
 
+import Monoid.\:
+
 trait Monoid[T] extends SemiGroup[T] with
-  def (a: T) \: (n: Long): T = if (n == 0) one else if (n % 2 == 0)
+  given Monoid[T] = this
+  def (a: T) \ (n: Long): T = if (n == 0) one else if (n % 2 == 0)
     val b = a \: (n / 2)
     b * b
   else
     a * a \: (n - 1)
   def (x: T) isOne: Boolean
   def one: T
+
+object Monoid with
+  def [T: Monoid](a: T) \: (n: Long): T = a \ n
