504c3d43dbb7c540bd94dfa78a733cbb19f90fef
Raphael Jolly <rjolly@users.sourceforge.net>
2020-02-16 17:20:10 +0100

    empty

--- scas/application/src/jas/package.scala
+++ scas/application/src/jas/package.scala
@@ -5,21 +5,25 @@ import edu.jas.poly.GenPolynomialRing
 import edu.jas.poly.GenPolynomial
 import edu.jas.structure.RingElem
 import edu.jas.structure.RingFactory
+import scala.collection.JavaConverters.asScalaBufferConverter
 
 package object jas with
-  given jas2scas[T <: RingElem[T] : RingFactory] as Ring[T] with Ordering[T] with
+  class JasRing[T <: RingElem[T]](val factory: RingFactory[T]) extends Ring[T] with Ordering[T] with
     def (x: T) + (y: T) = x.sum(y)
     def (x: T) - (y: T) = x.subtract(y)
     def (x: T) * (y: T) = x.multiply(y)
     def compare(x: T, y: T) = x.compareTo(y)
     def (x: T) isZero = x.isZERO()
     def (x: T) isOne = x.isONE()
-    def zero = summon[RingFactory[T]].getZERO()
-    def one = summon[RingFactory[T]].getONE()
+    def zero = factory.getZERO()
+    def one = factory.getONE()
 
-  given ZZ as BigInteger = BigInteger()
+  given ZZ as JasRing[BigInteger] = JasRing(BigInteger())
+
+  class PolyRing[C <: RingElem[C]](override val factory: GenPolynomialRing[C]) extends JasRing(factory) with
+    def gens = factory.getGenerators().asScala.toArray
 
   given int2bigInt as Conversion[Int, BigInteger] = BigInteger(_)
   given long2bigInt as Conversion[Long, BigInteger] = BigInteger(_)
-  given coef2poly[C <: RingElem[C]](using r: GenPolynomialRing[C]) as Conversion[C, GenPolynomial[C]] = r.valueOf(_)
-  given recurse2poly[U, C <: RingElem[C]](using r: GenPolynomialRing[C], c: Conversion[U, C]) as Conversion[U, GenPolynomial[C]] = r.valueOf(_)
+  given coef2poly[C <: RingElem[C]](using r: PolyRing[C]) as Conversion[C, GenPolynomial[C]] = r.factory.valueOf(_)
+  given recurse2poly[U, C <: RingElem[C]](using r: PolyRing[C], c: Conversion[U, C]) as Conversion[U, GenPolynomial[C]] = r.factory.valueOf(_)
