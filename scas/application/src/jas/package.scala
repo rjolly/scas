219b9fde3549a080a8c09cc0be361f4e39c91af8
Raphael Jolly <rjolly@users.sourceforge.net>
2020-02-17 18:37:04 +0100

    empty

--- scas/application/src/jas/package.scala
+++ scas/application/src/jas/package.scala
@@ -8,7 +8,8 @@ import edu.jas.structure.RingFactory
 import scala.collection.JavaConverters.asScalaBufferConverter
 
 package object jas with
-  class JasRing[T <: RingElem[T]](val factory: RingFactory[T]) extends Ring[T] with Ordering[T] with
+  class JasRing[T <: RingElem[T] : RingFactory] extends Ring[T] with Ordering[T] with
+    val factory = summon[RingFactory[T]]
     def (x: T) + (y: T) = x.sum(y)
     def (x: T) - (y: T) = x.subtract(y)
     def (x: T) * (y: T) = x.multiply(y)
@@ -18,14 +19,13 @@ package object jas with
     def zero = factory.getZERO()
     def one = factory.getONE()
 
-  given ZZ as JasRing[BigInteger] = JasRing(BigInteger())
-
-  class PolyRing[C <: RingElem[C]](override val factory: GenPolynomialRing[C]) extends JasRing(factory) with
+  given ZZ as BigInteger = BigInteger()
+  given j2s[T <: RingElem[T] : RingFactory] as JasRing[T]
+  given jas2scas[C <: RingElem[C] : GenPolynomialRing] as JasRing[GenPolynomial[C]] with
+    override val factory: GenPolynomialRing[C] = summon[GenPolynomialRing[C]]
     def gens = factory.getGenerators().asScala.toArray
-
-  given jas2scas[C <: RingElem[C]](using factory: GenPolynomialRing[C]) as PolyRing(factory)
 
   given int2bigInt as Conversion[Int, BigInteger] = BigInteger(_)
   given long2bigInt as Conversion[Long, BigInteger] = BigInteger(_)
-  given coef2poly[C <: RingElem[C]](using r: PolyRing[C]) as Conversion[C, GenPolynomial[C]] = r.factory.valueOf(_)
-  given recurse2poly[U, C <: RingElem[C]](using r: PolyRing[C], c: Conversion[U, C]) as Conversion[U, GenPolynomial[C]] = r.factory.valueOf(_)
+  given coef2poly[C <: RingElem[C] : GenPolynomialRing] as Conversion[C, GenPolynomial[C]] = summon[GenPolynomialRing[C]].valueOf(_)
+  given recurse2poly[U, C <: RingElem[C] : GenPolynomialRing](using Conversion[U, C]) as Conversion[U, GenPolynomial[C]] = summon[GenPolynomialRing[C]].valueOf(_)
