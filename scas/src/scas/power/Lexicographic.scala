bdb75bc115e82d999eee02ef21f4976567a7dbd4
Raphael Jolly <rjolly@users.sourceforge.net>
2020-04-02 22:00:02 +0200

    empty

--- scas/src/scas/power/Lexicographic.scala
+++ scas/src/scas/power/Lexicographic.scala
@@ -14,3 +14,27 @@ class Lexicographic[N : Numeric : ClassTag : ClassTagArray](vs: String*) extends
     0
   }
 }
+
+object Lexicographic {
+  inline def apply[N : Numeric : ClassTag : ClassTagArray](vs: String*) = new PowerProductWithDegree[N] {
+    val variables = vs.toArray
+    def compare(x: Array[N], y: Array[N]) = {
+      var i = length
+      while (i > 0) {
+        i -= 1
+        if (x(i) < y(i)) return -1
+        else if (x(i) > y(i)) return 1
+      }
+      0
+    }
+    override def (x: Array[N]) * (y: Array[N]) = {
+      val r = one
+      var i = 0
+      while (i <= length) {
+        r(i) = x(i) + y(i)
+        i += 1
+      }
+      r
+    }
+  }
+}
