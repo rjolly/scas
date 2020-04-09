2e8e62320deb20b53ebbd28263d48c7fc54deec2
Raphael Jolly <rjolly@users.sourceforge.net>
2020-04-09 11:08:22 +0200

    empty

--- scas/src/scas/power/PowerProduct.scala
+++ scas/src/scas/power/PowerProduct.scala
@@ -8,6 +8,7 @@ abstract class PowerProduct[N : Numeric : ClassTagArray] extends Monoid[Array[N]
   def variables: Array[String]
   given PowerProduct[N] = this
   def length = variables.length
+  def numeric = Numeric[N]
   def generator(variable: String): Array[N] = generator(variables.indexOf(variable))
   def generator(n: Int): Array[N]
   def generators = (for (i <- 0 until length) yield generator(i)).toArray
@@ -22,15 +23,15 @@ abstract class PowerProduct[N : Numeric : ClassTagArray] extends Monoid[Array[N]
   def (x: Array[N]) / (y: Array[N]): Array[N]
   def (x: Array[N]) | (y: Array[N]): Boolean
   def (x: Array[N]).isUnit = x >< 1
-  def dependencyOnVariables(x: Array[N]) = (for (i <- 0 until length if (x.get(i) > Numeric[N].zero)) yield i).toArray
+  def dependencyOnVariables(x: Array[N]) = (for (i <- 0 until length if (x.get(i) > numeric.zero)) yield i).toArray
   def (x: Array[N]).projection(n: Int): Array[N]
   def (x: Array[N]).toCode(level: Level) = {
     var s = "1"
     var m = 0
-    for (i <- 0 until length) if (x.get(i) > Numeric[N].zero) {
+    for (i <- 0 until length) if (x.get(i) > numeric.zero) {
       val a = variables(i)
       val b = x.get(i)
-      val t = if (b >< Numeric[N].one) a else s"$a\\$b"
+      val t = if (b >< numeric.one) a else s"$a\\$b"
       s = if (m == 0) t else s"$s*$t"
       m += 1
     }
@@ -39,10 +40,10 @@ abstract class PowerProduct[N : Numeric : ClassTagArray] extends Monoid[Array[N]
   def (x: Array[N]).toMathML = {
     var s = "<cn>1</cn>"
     var m = 0
-    for (i <- 0 until length) if (x.get(i) > Numeric[N].zero) {
+    for (i <- 0 until length) if (x.get(i) > numeric.zero) {
       val a = variables(i)
       val b = x.get(i)
-      val t = if (b >< Numeric[N].one) s"<ci>$a</ci>" else s"<apply><power/><ci>$a</ci><cn>$b</cn></apply>"
+      val t = if (b >< numeric.one) s"<ci>$a</ci>" else s"<apply><power/><ci>$a</ci><cn>$b</cn></apply>"
       s = if (m == 0) t else s"<apply><times/>$s$t</apply>"
       m += 1
     }
@@ -52,7 +53,7 @@ abstract class PowerProduct[N : Numeric : ClassTagArray] extends Monoid[Array[N]
   def (x: Array[N]).convert(from: Array[String]): Array[N]
   def size(x: Array[N]) = {
     var m = 0
-    for (i <- 0 until length) if (x.get(i) > Numeric[N].zero) m += 1
+    for (i <- 0 until length) if (x.get(i) > numeric.zero) m += 1
     m
   }
   def (x: Array[N]).get(i: Int): N
--- scas/src/scas/power/PowerProductWithDegree.scala
+++ scas/src/scas/power/PowerProductWithDegree.scala
@@ -6,14 +6,14 @@ abstract class PowerProductWithDegree[N : Numeric : ClassTag : ClassTagArray] ex
   def one = new Array[N](length + 1)
   def generator(n: Int) = {
     val r = one
-    for (i <- 0 to length) r(i) = Numeric[N].fromInt(if (i == n || i == length) 1 else 0)
+    for (i <- 0 to length) r(i) = numeric.fromInt(if (i == n || i == length) 1 else 0)
     r
   }
-  def degree(x: Array[N]) = Numeric[N].toLong(x(length))
+  def degree(x: Array[N]) = numeric.toLong(x(length))
   def gcd(x: Array[N], y: Array[N]): Array[N] = {
     val r = one
     for (i <- 0 until length) {
-      r(i) = Numeric[N].min(x(i), y(i))
+      r(i) = numeric.min(x(i), y(i))
       r(length) += r(i)
     }
     r
@@ -21,7 +21,7 @@ abstract class PowerProductWithDegree[N : Numeric : ClassTag : ClassTagArray] ex
   def scm(x: Array[N], y: Array[N]): Array[N] = {
     val r = one
     for (i <- 0 until length) {
-      r(i) = Numeric[N].max(x(i), y(i))
+      r(i) = numeric.max(x(i), y(i))
       r(length) += r(i)
     }
     r
@@ -53,13 +53,13 @@ abstract class PowerProductWithDegree[N : Numeric : ClassTag : ClassTagArray] ex
   }
   def (x: Array[N]).projection(n: Int) = {
     val r = one
-    for (i <- 0 to length) r(i) = if (i == n || i == length) x(n) else Numeric[N].zero
+    for (i <- 0 to length) r(i) = if (i == n || i == length) x(n) else numeric.zero
     r
   }
   def (x: Array[N]).convert(from: Array[String]) = {
     val r = one
     val index = from.map(a => variables.indexOf(a))
-    for (i <- 0 until x.length - 1) if (x(i) > Numeric[N].zero) {
+    for (i <- 0 until x.length - 1) if (x(i) > numeric.zero) {
       val c = index(i)
       assert (c > -1)
       r(c) = x(i)
