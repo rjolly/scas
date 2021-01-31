b4445f0a35866ce88eb62ee1967acc5b5c8263fd
Raphael Jolly <rjolly@users.sourceforge.net>
2021-01-31 20:02:13 +0100

    empty

--- scas/src/scas/structure/Module.scala
+++ scas/src/scas/structure/Module.scala
@@ -4,7 +4,9 @@ import scas.structure.Ring
 
 abstract class Module[T, R: Ring] extends AbelianGroup[T] {
   extension[U] (x: U)(using c: U => R) def *%(y: T): T = c(x) *%y
-  extension[U] (x: U)(using c: U => T) def %* (y: R): T = c(x)%* y
   extension (x: R) def *%(y: T): T
-  extension (x: T) def %* (y: R): T
+  extension (x: T) {
+    def %*[U] (y: U)(using c: U => R): T = x%* c(y)
+    def %* (y: R): T
+  }
 }
