46b26da6e3d0ceb1491c5942480a7602774a11ad
Raphael Jolly <rjolly@users.sourceforge.net>
2021-02-26 19:26:54 +0100

    empty

--- scas/application/src/jas/Main.scala
+++ scas/application/src/jas/Main.scala
@@ -27,10 +27,8 @@ class poly2scas[C <: RingElem[C] : GenPolynomialRing] extends Ring[GenPolynomial
 
 given int2bigInt: Conversion[Int, BigInteger] = new BigInteger(_)
 given long2bigInt: Conversion[Long, BigInteger] = new BigInteger(_)
-given coef2poly[U, C <: RingElem[C] : GenPolynomialRing](using c: U => C): Conversion[U, GenPolynomial[C]] = x => coef2poly.apply(c(x))
-given coef2poly[C <: RingElem[C] : GenPolynomialRing]: Conversion[C, GenPolynomial[C]] = summon[GenPolynomialRing[C]].valueOf(_)
+given coef2poly[U, C <: RingElem[C] : GenPolynomialRing](using c: U => C): Conversion[U, GenPolynomial[C]] = x => summon[GenPolynomialRing[C]].valueOf(c(x))
 
-given bigInt2scas[U](using c: U => BigInteger): Conversion[U, scas.BigInteger] = x => bigInt2scas.apply(c(x))
-given bigInt2scas: Conversion[BigInteger, scas.BigInteger] = _.`val`
+given bigInt2scas[U](using c: U => BigInteger): Conversion[U, scas.BigInteger] = x => (c(x)).`val`
 
 extension (a: Long) def \:(b: Long) = long2bigInt(a) \ bigInt2scas.apply(long2bigInt(b))
--- scas/application/src/rings/Main.scala
+++ scas/application/src/rings/Main.scala
@@ -16,10 +16,8 @@ import BigInteger.given
 
 given int2bigInt: Conversion[Int, BigInteger] = cc.redberry.rings.bigint.BigInteger.valueOf(_)
 given long2bigInt: Conversion[Long, BigInteger] = cc.redberry.rings.bigint.BigInteger.valueOf(_)
-given coef2poly[U, C : MultivariatePolynomialRing](using c: U => C): Conversion[U, MultivariatePolynomial[C]] = x => coef2poly.apply(c(x))
-given coef2poly[C : MultivariatePolynomialRing]: Conversion[C, MultivariatePolynomial[C]] = MultivariatePolynomialRing[C].ring.factory().createConstant(_)
+given coef2poly[U, C : MultivariatePolynomialRing](using c: U => C): Conversion[U, MultivariatePolynomial[C]] = x => MultivariatePolynomialRing[C].ring.factory().createConstant(c(x))
 
-given bigInt2scas[U](using c: U => BigInteger): Conversion[U, scas.BigInteger] = x => bigInt2scas.apply(c(x))
-given bigInt2scas: Conversion[BigInteger, scas.BigInteger] = x => java.math.BigInteger(x.toByteArray)
+given bigInt2scas[U](using c: U => BigInteger): Conversion[U, scas.BigInteger] = x => java.math.BigInteger(c(x).toByteArray)
 
 extension (a: Long) def \:(b: Long) = long2bigInt(a) \ bigInt2scas.apply(long2bigInt(b))
