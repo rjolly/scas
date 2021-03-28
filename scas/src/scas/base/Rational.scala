7f6e29133214cb1eccf59e95643427c1bca3d313
Raphael Jolly <rjolly@users.sourceforge.net>
2021-03-28 20:12:15 +0200

    Revert "empty"
    This reverts commit 5b611643e6f2a3bfa80a89c9999993a32621b66b.

--- scas/src/scas/base/Rational.scala
+++ scas/src/scas/base/Rational.scala
@@ -9,7 +9,7 @@ type Rational = Quotient.Element[BigInteger]
 object Rational extends Quotient[BigInteger] with Field[Rational] {
   given Rational.type = this
   object Implicits {
-    given bigInt2rational[U](using c: U => BigInteger): (U => Rational) = x => Rational(c(x))
+    given bigInt2rational[U](using c: U => BigInteger): (U => Rational) = x => fromRing(c(x))
   }
   def compare(x: Rational, y: Rational) = {
     val Rational(a, b) = x
