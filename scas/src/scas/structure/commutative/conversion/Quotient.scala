febc67e174245aeca6e6e387bc28ed68279a987e
Raphael Jolly <rjolly@users.sourceforge.net>
2021-03-30 15:20:01 +0200

    empty

--- scas/src/scas/structure/commutative/conversion/Quotient.scala
+++ scas/src/scas/structure/commutative/conversion/Quotient.scala
@@ -1,3 +1,7 @@
 package scas.structure.commutative.conversion
 
-trait Quotient[T: scas.structure.commutative.UniqueFactorizationDomain] extends scas.structure.commutative.Quotient[T] with Field[scas.structure.commutative.Quotient.Element[T]]
+import Quotient.Element
+
+trait Quotient[T: scas.structure.commutative.UniqueFactorizationDomain] extends scas.structure.commutative.Quotient[T] with Field[Element[T]]
+
+val Quotient = scas.structure.commutative.Quotient
--- scas/src/scas/structure/commutative/conversion/Residue.scala
+++ scas/src/scas/structure/commutative/conversion/Residue.scala
@@ -1,3 +1,5 @@
 package scas.structure.commutative.conversion
 
-trait Residue[T: scas.structure.commutative.UniqueFactorizationDomain] extends scas.structure.commutative.Residue[T] with UniqueFactorizationDomain[T]
+trait Residue[T: scas.structure.commutative.UniqueFactorizationDomain] extends scas.structure.commutative.Residue[T] with UniqueFactorizationDomain[T] {
+  override def ring = scas.structure.commutative.UniqueFactorizationDomain[T]
+}
