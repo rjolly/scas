9bf6ee5e3a27551778c7e4bd07520388fb765b06
Raphael Jolly <rjolly@users.sourceforge.net>
2021-04-02 22:45:46 +0200

    empty

--- scas/src/scas/polynomial/conversion/Polynomial.scala
+++ scas/src/scas/polynomial/conversion/Polynomial.scala
@@ -5,7 +5,9 @@ import scas.structure.Ring
 import scas.power.PowerProduct
 
 abstract class Polynomial[T : ClassTag, C : Ring, M : PowerProduct] extends scas.polynomial.Polynomial[T, C, M] with scas.structure.conversion.Ring[T] {
+  given Polynomial[T, C, M] = this
   extension (x: T) {
     def %*[U] (m: U)(using c: U => M): T = super.%*(x)(c(m))
   }
+  given coef2poly[D](using c: D => C): (D => T) = x => this(c(x))
 }
