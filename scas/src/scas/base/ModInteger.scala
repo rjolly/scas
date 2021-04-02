173d3804b3d63c9c989b2e07b06fdb0d1a70554a
Raphael Jolly <rjolly@users.sourceforge.net>
2021-04-02 17:24:38 +0200

    empty

--- scas/src/scas/base/ModInteger.scala
+++ scas/src/scas/base/ModInteger.scala
@@ -1,14 +1,11 @@
 package scas.base
 
-import scas.structure.commutative.conversion.Residue
-import scas.structure.commutative.ordered.conversion.Field
+import scas.structure.commutative.ordered.conversion.{Residue, Field}
 
 class ModInteger(val mod: BigInteger) extends Residue(using BigInteger) with Field[BigInteger] {
   given ModInteger = this
   assert (mod.isProbablePrime(100))
   override def apply(x: BigInteger) = x.mod(mod)
-  def compare(x: BigInteger, y: BigInteger) = BigInteger.compare(x, y)
-  extension (x: BigInteger) override def signum = super.signum(x)
   def characteristic = mod
   extension (a: BigInteger) override def pow(b: BigInteger) = a.modPow(b, mod)
   def inverse(x: BigInteger) = x.modInverse(mod)
--- scas/src/scas/base/Rational.scala
+++ scas/src/scas/base/Rational.scala
@@ -1,24 +1,17 @@
 package scas.base
 
-import scas.structure.commutative.conversion.Quotient
-import scas.structure.commutative.ordered.conversion.Field
+import scas.structure.commutative.ordered.conversion.Quotient
 import BigInteger.given
 
 type Rational = Quotient.Element[BigInteger]
 
-object Rational extends Quotient[BigInteger] with Field[Rational] {
+object Rational extends Quotient[BigInteger] {
   given Rational.type = this
   object Implicits {
     given bigInt2rational[U](using c: U => BigInteger): (U => Rational) = x => Rational(c(x))
   }
   def apply(n: String, d: String): Rational = this(BigInteger(n), BigInteger(d))
-  def compare(x: Rational, y: Rational) = {
-    val Rational(a, b) = x
-    val Rational(c, d) = y
-    BigInteger.compare(a * d, c * b)
-  }
   extension (a: Long) def %%(b: Long) = this(long2bigInt(a), long2bigInt(b))
-  extension (x: Rational) override def signum = super.signum(x)
   extension (x: Rational) override def toCode(level: Level) = {
     val Rational(n, d) = x
     if (d.isOne) n.toCode(level) else {
