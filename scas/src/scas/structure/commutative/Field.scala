35a43567e5945a88101143818979ce70c4cc7525
Raphael Jolly <rjolly@users.sourceforge.net>
2021-04-04 13:15:49 +0200

    empty

--- scas/src/scas/structure/commutative/Field.scala
+++ scas/src/scas/structure/commutative/Field.scala
@@ -1,9 +1,9 @@
 package scas.structure.commutative
 
-trait Field[T] extends EuclidianDomain[T] with scas.structure.Field[T] {
+trait Field[T] extends scas.structure.Field[T] with EuclidianDomain[T] {
   override def gcd(x: T, y: T) = if (y.isZero) x else y
   extension (x: T) {
-    override def divide(y: T) = super[Field]./(x)(y)
+    override def divide(y: T) = super[Field].divide(x)(y)
     override def remainder(y: T) = zero
     override def divideAndRemainder(y: T) = (x / y, x % y)
   }
--- scas/src/scas/structure/commutative/conversion/UniqueFactorizationDomain.scala
+++ scas/src/scas/structure/commutative/conversion/UniqueFactorizationDomain.scala
@@ -1,6 +1,9 @@
 package scas.structure.commutative.conversion
 
 trait UniqueFactorizationDomain[T] extends scas.structure.commutative.UniqueFactorizationDomain[T] with scas.structure.conversion.NotQuiteField[T] {
+  abstract override def gcd(x: T, y: T) = super.gcd(x, y)
+  def gcd[U, V](x: U, y: V)(using c: U => T, d: V => T): T = gcd(c(x), d(y))
+  def lcm[U, V](x: U, y: V)(using c: U => T, d: V => T) = super.lcm(c(x), d(y))
   extension[U] (x: U)(using c: U => T) {
     def % (y: T) = c(x).remainder(y)
     def /%(y: T) = c(x).divideAndRemainder(y)
