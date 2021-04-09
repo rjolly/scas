3b4ad289b69e66f624dffde2f0bd8546acda8d77
Raphael Jolly <rjolly@users.sourceforge.net>
2021-04-09 13:19:51 +0200

    empty

--- scas/application/src/jas/Main.scala
+++ scas/application/src/jas/Main.scala
@@ -1,6 +1,10 @@
 package jas
 
 import edu.jas.poly.GenPolynomialRing
+import edu.jas.poly.GenPolynomial
 import edu.jas.structure.RingElem
+import scas.util.{Conversion, unary_~}
 
-extension [C <: RingElem[C]](factory: GenPolynomialRing[C]) def toScas = new PolynomialRing(factory)
+given poly2scas[C <: RingElem[C] : GenPolynomialRing]: PolynomialRing[C] with {}
+
+given coef2poly[C <: RingElem[C] : GenPolynomialRing, D : Conversion[C]]: (D => GenPolynomial[C]) = x => summon[GenPolynomialRing[C]].valueOf(~x)
--- scas/application/src/jas/PolynomialRing.scala
+++ scas/application/src/jas/PolynomialRing.scala
@@ -4,11 +4,12 @@ import edu.jas.poly.GenPolynomialRing
 import edu.jas.poly.GenPolynomial
 import edu.jas.structure.RingElem
 import scala.collection.JavaConverters.asScalaBufferConverter
-import scas.util.{Conversion, unary_~}
+import scas.util.Conversion
 
-class PolynomialRing[C <: RingElem[C]](val factory: GenPolynomialRing[C]) extends Ring[GenPolynomial[C]] {
+class PolynomialRing[C <: RingElem[C] : GenPolynomialRing] extends Ring[GenPolynomial[C]] {
   given PolynomialRing[C] = this
+  val factory: GenPolynomialRing[C] = summon[GenPolynomialRing[C]]
   def gens = factory.getGenerators().asScala.toArray
 
-  given coef2poly[D: Conversion[C]]: (D => GenPolynomial[C]) = x => factory.valueOf(~x)
+  given coef2poly[D: Conversion[C]]: (D => GenPolynomial[C]) = jas.coef2poly
 }
