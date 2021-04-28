8c24150f3f62100001a0844ff3e8aa2a29567d7e
Raphael Jolly <rjolly@users.sourceforge.net>
2021-04-28 11:38:47 +0200

    empty

--- scas/src/scas/power/conversion/Lexicographic.scala
+++ scas/src/scas/power/conversion/Lexicographic.scala
@@ -2,10 +2,12 @@ package scas.power.conversion
 
 import scala.reflect.ClassTag
 import scas.math.Numeric
-import scas.util.ClassTagArray
+import scas.util.{ClassTagArray, Conversion, unary_~}
 import scas.variable.Variable
-import Variable.string2variable
+import scas.power.Lexicographic.Impl
 
-class Lexicographic[N : Numeric : ClassTag : ClassTagArray](variables: Variable*) extends scas.power.Lexicographic[N](variables: _*) with PowerProduct[Array[N]] {
-  def this(variables: String*)(using DummyImplicit) = this(variables.map(string2variable): _*)
+class Lexicographic[N : Numeric : ClassTag : ClassTagArray](variables: Variable*) extends scas.power.Lexicographic[N](variables: _*) with PowerProduct[Array[N]]
+
+object Lexicographic extends Impl {
+  def apply[N : Numeric : ClassTag : ClassTagArray, U: Conversion[Variable]](degree: N)(variables: U*) = new Lexicographic[N](variables.map(~_): _*)
 }
