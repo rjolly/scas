ed6f35fb934de78cb35e807aa25b41b7443380d9
Raphael Jolly <rjolly@users.sourceforge.net>
2021-05-01 14:36:50 +0200

    empty

--- scas/src/scas/structure/commutative/conversion/UniqueFactorizationDomain.scala
+++ scas/src/scas/structure/commutative/conversion/UniqueFactorizationDomain.scala
@@ -6,7 +6,8 @@ import scas.util.{Conversion, unary_~}
 trait UniqueFactorizationDomain[T] extends scas.structure.commutative.UniqueFactorizationDomain[T] with scas.structure.conversion.NotQuiteField[T] {
   abstract override def gcd(x: T, y: T) = super.gcd(x, y)
   def gcd[U: Conversion[T], V: Conversion[T]](x: U, y: V): T = gcd(~x, ~y)
-  def lcm[U: Conversion[T], V: Conversion[T]](x: U, y: V) = super.lcm(~x, ~y)
+  def lcm[U: Conversion[T], V: Conversion[T]](x: U, y: V): T = lcm(~x, ~y)
+  override def lcm(x: T, y: T) = super.lcm(x, y)
   extension[U: Conversion[T]] (x: U) {
     def % (y: T) = (~x).remainder(y)
     def /%(y: T) = (~x).divideAndRemainder(y)
