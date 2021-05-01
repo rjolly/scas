f8f3907287a02806f20e47a18f970f09c114fd97
Raphael Jolly <rjolly@users.sourceforge.net>
2021-05-01 11:29:26 +0200

    empty

--- scas/src/scas/power/Lexicographic.scala
+++ scas/src/scas/power/Lexicographic.scala
@@ -4,8 +4,6 @@ import scala.reflect.ClassTag
 import scas.math.Numeric
 import scas.util.ClassTagArray
 import scas.variable.Variable
-import Variable.string2variable
-import Lexicographic.Impl
 
 class Lexicographic[N : Numeric : ClassTag : ClassTagArray](val variables: Variable*) extends ArrayPowerProductWithDegree[N] {
   def compare(x: Array[N], y: Array[N]) = {
@@ -16,30 +14,5 @@ class Lexicographic[N : Numeric : ClassTag : ClassTagArray](val variables: Varia
       else if (x(i) > y(i)) return 1
     }
     0
-  }
-}
-
-object Lexicographic extends Impl {
-  class Impl {
-    inline def apply[N : Numeric : ClassTag : ClassTagArray](variables: String*) = new Lexicographic[N](variables.map(string2variable): _*) {
-      override def compare(x: Array[N], y: Array[N]) = {
-        var i = length
-        while (i > 0) {
-          i -= 1
-          if (x(i) < y(i)) return -1
-          else if (x(i) > y(i)) return 1
-        }
-        0
-      }
-      extension (x: Array[N]) override def multiply(y: Array[N]) = {
-        val r = one
-        var i = 0
-        while (i <= length) {
-          r(i) = x(i) + y(i)
-          i += 1
-        }
-        r
-      }
-    }
   }
 }
--- scas/src/scas/power/conversion/Lexicographic.scala
+++ scas/src/scas/power/conversion/Lexicographic.scala
@@ -4,10 +4,31 @@ import scala.reflect.ClassTag
 import scas.math.Numeric
 import scas.util.{ClassTagArray, Conversion, unary_~}
 import scas.variable.Variable
-import scas.power.Lexicographic.Impl
+import Variable.string2variable
 
 class Lexicographic[N : Numeric : ClassTag : ClassTagArray](variables: Variable*) extends scas.power.Lexicographic[N](variables: _*) with PowerProduct[Array[N]]
 
-object Lexicographic extends Impl {
+object Lexicographic {
   def apply[N : Numeric : ClassTag : ClassTagArray, U: Conversion[Variable]](degree: N)(variables: U*) = new Lexicographic[N](variables.map(~_): _*)
+
+  inline def apply[N : ClassTag : ClassTagArray](using numeric: Numeric[N])(variables: String*) = new Lexicographic[N](variables.map(string2variable): _*) {
+    override def compare(x: Array[N], y: Array[N]) = {
+      var i = length
+      while (i > 0) {
+        i -= 1
+        if (numeric.<(x(i))(y(i))) return -1
+        else if (numeric.>(x(i))(y(i))) return 1
+      }
+      0
+    }
+    extension (x: Array[N]) override def multiply(y: Array[N]) = {
+      val r = one
+      var i = 0
+      while (i <= length) {
+        r(i) = x(i) + y(i)
+        i += 1
+      }
+      r
+    }
+  }
 }
