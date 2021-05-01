92a620a1cdd823a0eb7f99dc1029daed7d4d6023
Raphael Jolly <rjolly@users.sourceforge.net>
2021-05-01 11:53:40 +0200

    empty

--- scas/src/scas/math/Equiv.scala
+++ scas/src/scas/math/Equiv.scala
@@ -2,7 +2,7 @@ package scas.math
 
 trait Equiv[T] extends scala.math.Equiv[T] {
   extension (x: T) {
-    def ><(y: T) = equiv(x, y)
-    def <>(y: T) = !equiv(x, y)
+    inline def ><(y: T) = equiv(x, y)
+    inline def <>(y: T) = !equiv(x, y)
   }
 }
