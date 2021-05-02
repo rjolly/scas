b36c2a2029b77cb95c1708cf370e41ed7fa65788
Raphael Jolly <rjolly@users.sourceforge.net>
2021-05-02 11:12:44 +0200

    empty

--- scas/src/scas/structure/Product.scala
+++ scas/src/scas/structure/Product.scala
@@ -2,69 +2,64 @@ package scas.structure
 
 import scas.base.conversion.BigInteger
 import BigInteger.lcm
-import Product.Element
 
-class Product[R1, R2](using ring1: Ring[R1], ring2: Ring[R2]) extends Ring[Element[R1, R2]] {
-  def apply(a: R1, b: R2) = Element(a, b)
-  override def convert(x: Element[R1, R2]) = {
-    val Element(a, b) = x
-    Element(ring1.convert(a), ring2.convert(b))
+class Product[R1, R2](using ring1: Ring[R1], ring2: Ring[R2]) extends Ring[(R1, R2)] {
+  def apply(a: R1, b: R2) = (a, b)
+  override def convert(x: (R1, R2)) = {
+    val (a, b) = x
+    (ring1.convert(a), ring2.convert(b))
   }
-  extension (x: Element[R1, R2]) def add(y: Element[R1, R2]) = {
-    val Element(a, b) = x
-    val Element(c, d) = y
-    Element(a + c, b + d)
+  extension (x: (R1, R2)) def add(y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    (a + c, b + d)
   }
-  extension (x: Element[R1, R2]) def subtract(y: Element[R1, R2]) = {
-    val Element(a, b) = x
-    val Element(c, d) = y
-    Element(a - c, b - d)
+  extension (x: (R1, R2)) def subtract(y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    (a - c, b - d)
   }
-  extension (x: Element[R1, R2]) def multiply(y: Element[R1, R2]) = {
-    val Element(a, b) = x
-    val Element(c, d) = y
-    Element(a * c, b * d)
+  extension (x: (R1, R2)) def multiply(y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    (a * c, b * d)
   }
-  def equiv(x: Element[R1, R2], y: Element[R1, R2]) = {
-    val Element(a, b) = x
-    val Element(c, d) = y
+  def equiv(x: (R1, R2), y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
     a >< c && b >< d
   }
-  extension (x: Element[R1, R2]) override def unary_- = {
-    val Element(a, b) = x
-    Element(-a, -b)
+  extension (x: (R1, R2)) override def unary_- = {
+    val (a, b) = x
+    (-a, -b)
   }
-  extension (a: Element[R1, R2]) override def pow(b: BigInteger) = {
-    val Element(c, d) = a
-    Element(c \ b, d \ b)
+  extension (a: (R1, R2)) override def pow(b: BigInteger) = {
+    val (c, d) = a
+    (c \ b, d \ b)
   }
-  extension (x: Element[R1, R2]) def isUnit = {
-    val Element(a, b) = x
+  extension (x: (R1, R2)) def isUnit = {
+    val (a, b) = x
     a.isUnit && b.isUnit
   }
-  override def abs(x: Element[R1, R2]) = {
-    val Element(a, b) = x
-    Element(ring1.abs(a), ring2.abs(b))
+  override def abs(x: (R1, R2)) = {
+    val (a, b) = x
+    (ring1.abs(a), ring2.abs(b))
   }
-  extension (x: Element[R1, R2]) def signum = {
-    val Element(a, b) = x
+  extension (x: (R1, R2)) def signum = {
+    val (a, b) = x
     if (a.signum == 0) b.signum else a.signum
   }
   def characteristic = lcm(ring1.characteristic, ring2.characteristic)
-  extension (x: Element[R1, R2]) def toCode(level: Level) = {
-    val Element(a, b) = x
+  extension (x: (R1, R2)) def toCode(level: Level) = {
+    val (a, b) = x
     s"Product(${a.toCode(Level.Addition)}, ${b.toCode(Level.Addition)})"
   }
   override def toString = s"Product($ring1, $ring2)"
-  extension (x: Element[R1, R2]) def toMathML = {
-    val Element(a, b) = x
+  extension (x: (R1, R2)) def toMathML = {
+    val (a, b) = x
     s"<apply><cartesianproduct/>${a.toMathML}${b.toMathML}</apply>"
   }
   def toMathML = s"<apply><cartesianproduct/>${ring1.toMathML}${ring2.toMathML}</apply>"
-  def zero = Element(ring1.zero, ring2.zero)
-  def one = Element(ring1.one, ring2.one)
-}
-
-object Product {
-  case class Element[R1, R2](_1: R1, _2: R2)
+  def zero = (ring1.zero, ring2.zero)
+  def one = (ring1.one, ring2.one)
 }
--- scas/src/scas/structure/conversion/Product.scala
+++ scas/src/scas/structure/conversion/Product.scala
@@ -1,9 +1,8 @@
 package scas.structure.conversion
 
-import scas.structure.Product.Element
 import scas.util.{Conversion, unary_~}
 
-class Product[R1: scas.structure.Ring, R2: scas.structure.Ring] extends scas.structure.Product[R1, R2] with Ring[Element[R1, R2]] {
+class Product[R1: scas.structure.Ring, R2: scas.structure.Ring] extends scas.structure.Product[R1, R2] with Ring[(R1, R2)] {
   given Product[R1, R2] = this
 }
 
