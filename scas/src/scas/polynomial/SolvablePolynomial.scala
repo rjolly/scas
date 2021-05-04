2db8e36d436518697e02d22bbb968e1b1466c65d
Raphael Jolly <rjolly@users.sourceforge.net>
2021-05-04 14:51:38 +0200

    empty

--- scas/src/scas/polynomial/SolvablePolynomial.scala
+++ scas/src/scas/polynomial/SolvablePolynomial.scala
@@ -11,13 +11,16 @@ import scas.power.PowerProduct
 trait SolvablePolynomial[T : ClassTag, C : Ring, M](using pp: PowerProduct[M]) extends Polynomial[T, C, M] {
   import pp.dependencyOnVariables
   type Key = (Int, Int)
-  type Relation = (M, M, T)
+  case class Relation(e: M, f: M, p: T) {
+    override def toString = s"${p.show}-${e.show}*${f.show}"
+    def toMathML = s"<apply><minus/>${p.toMathML}<apply><times/>${e.toMathML}${f.toMathML}</apply></apply>"
+  }
   val table = new TreeMap[Key, List[Relation]](Ordering[Key])
   def update(e: T, f: T, p: T): Unit = update(headPowerProduct(e), headPowerProduct(f), p)
   def update(e: M, f: M, p: T) = {
     val key = makeKey(e, f)
     val list = table.asScala.getOrElse(key, Nil)
-    table.put(key, insert(list, (e, f, p)))
+    table.put(key, insert(list, Relation(e, f, p)))
   }
   def insert(list: List[Relation], relation: Relation): List[Relation] = list match {
     case head::tail if (factorOf(relation, head)) => head::insert(tail, relation)
@@ -27,10 +30,10 @@ trait SolvablePolynomial[T : ClassTag, C : Ring, M](using pp: PowerProduct[M]) e
     val key = makeKey(e, f)
     val list = table.asScala.getOrElse(key, Nil)
     list match {
-      case Nil => (pp.one, pp.one, this(e * f))
+      case Nil => Relation(pp.one, pp.one, this(e * f))
       case _ => {
-        val (e0, f0, p0) = select(list, (e, f, zero))
-        (e / e0, f / f0, p0)
+        val Relation(e0, f0, p0) = select(list, Relation(e, f, zero))
+        Relation(e / e0, f / f0, p0)
       }
     }
   }
@@ -39,8 +42,8 @@ trait SolvablePolynomial[T : ClassTag, C : Ring, M](using pp: PowerProduct[M]) e
     case _ => relation
   }
   def factorOf(x: Relation, y: Relation) = {
-    val (ex, fx, px) = x
-    val (ey, fy, py) = y
+    val Relation(ex, fx, px) = x
+    val Relation(ey, fy, py) = y
     (ex | ey) && (fx | fy)
   }
   def makeKey(e: M, f: M) = {
@@ -48,16 +51,8 @@ trait SolvablePolynomial[T : ClassTag, C : Ring, M](using pp: PowerProduct[M]) e
     val df = dependencyOnVariables(f)
     (de(0), df(0))
   }
-  override def toString = s"${super.toString}(${(for ((a, b) <- table.asScala; relation <- b) yield toString(relation)).mkString(", ")})"
-  def toString(relation: Relation) = {
-    val (e, f, p) = relation
-    s"${p.show}-${e.show}*${f.show}"
-  }
-  override def toMathML = s"<apply>${super.toMathML}<list>${(for ((a, b) <- table.asScala; relation <- b) yield toMathML(relation)).mkString}</list></apply>"
-  def toMathML(relation: Relation): String = {
-    val (e, f, p) = relation
-    s"<apply><minus/>${p.toMathML}<apply><times/>${e.toMathML}${f.toMathML}</apply></apply>"
-  }
+  override def toString = s"${super.toString}(${(for ((a, b) <- table.asScala; relation <- b) yield relation.toString).mkString(", ")})"
+  override def toMathML = s"<apply>${super.toMathML}<list>${(for ((a, b) <- table.asScala; relation <- b) yield relation.toMathML).mkString}</list></apply>"
 
   extension (ring: Polynomial[T, C, M]) def apply(s: T*): SolvablePolynomial[T, C, M] = {
     assert (s.foldLeft(true)((l, r) => l && r.isZero))
@@ -88,7 +83,7 @@ trait SolvablePolynomial[T : ClassTag, C : Ring, M](using pp: PowerProduct[M]) e
         val f2 = f.projection(fl)
         val e1 = e / e2
         val f1 = f / f2
-        val (e3, f3, c3) = lookup(e2, f2)
+        val Relation(e3, f3, c3) = lookup(e2, f2)
         var cs = c3
         if (!(f3.isOne)) {
           cs = cs%* f3
