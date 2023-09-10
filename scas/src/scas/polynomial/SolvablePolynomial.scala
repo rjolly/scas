31438c3cb544c52d04bbc1dbc92e8199000a5c21
Raphael Jolly <rjolly@users.sourceforge.net>
2023-09-10 09:40:53 +0200

    empty

--- scas/src/scas/polynomial/SolvablePolynomial.scala
+++
@@ -1,7 +0,0 @@
-package scas.polynomial
-
-import scala.reflect.ClassTag
-import scas.structure.impl.Ring
-import scas.power.impl.PowerProduct
-
-trait SolvablePolynomial[T : ClassTag, C : Ring, M : PowerProduct] extends impl.SolvablePolynomial[T, C, M] with Polynomial[T, C, M]
--- scas/src/scas/polynomial/tree/SolvablePolynomial.scala
+++ scas/src/scas/polynomial/tree/SolvablePolynomial.scala
@@ -2,9 +2,10 @@ package scas.polynomial.tree
 
 import scas.structure.impl.Ring
 import scas.power.impl.PowerProduct
+import scas.polynomial.Polynomial
 import scas.polynomial.impl.TreePolynomial
 import scas.polynomial.TreePolynomial.Element
 
-class SolvablePolynomial[C : Ring, M : PowerProduct] extends TreePolynomial[C, M] with scas.polynomial.SolvablePolynomial[Element[C, M], C, M] {
+class SolvablePolynomial[C : Ring, M : PowerProduct] extends TreePolynomial[C, M] with scas.polynomial.impl.SolvablePolynomial[Element[C, M], C, M] with Polynomial[Element[C, M], C, M] {
   given instance: SolvablePolynomial[C, M] = this
 }
