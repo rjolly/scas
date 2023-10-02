d3a32b1edc80c656292c15dbe982cd2c62496248
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-02 16:28:23 +0200

    empty

--- scas/src/scas/polynomial/ufd/MultivariatePolynomial.scala
+++ scas/src/scas/polynomial/ufd/MultivariatePolynomial.scala
@@ -6,16 +6,16 @@ import scas.structure.commutative.impl.UniqueFactorizationDomain
 
 trait MultivariatePolynomial[T[C, M], C, M](using ClassTag[T[C, M]])(using ring: UniqueFactorizationDomain[C], pp: PowerProduct[M]) extends PolynomialOverUFD[T[C, M], C, M] {
   val location = variables.length - 1
-  given split: MultivariatePolynomial[T, T[C, M], M]
+  def split: MultivariatePolynomial[T, T[C, M], M]
   override def gcd(x: T[C, M], y: T[C, M]) = if (location > 0) {
     val s = split
-    s.gcd(x.convertTo(s), y.convertTo(s)).convertFrom(s)
+    s.gcd(x.convertTo(using s), y.convertTo(using s)).convertFrom(s)
   } else {
     val (a, p) = contentAndPrimitivePart(x)
     val (b, q) = contentAndPrimitivePart(y)
     primitivePart(gcd1(p, q)).coefMultiply(ring.gcd(a, b))
   }
-  extension (x: T[C, M]) def convertTo(s: MultivariatePolynomial[T, T[C, M], M]): T[T[C, M], M] = iterator(x).foldLeft(s.zero) { (l, r) =>
+  extension (x: T[C, M]) def convertTo(using s: MultivariatePolynomial[T, T[C, M], M]): T[T[C, M], M] = iterator(x).foldLeft(s.zero) { (l, r) =>
     val (m, c) = r
     val t = m.projection(location)
     l + s(t, this(m / t, c)).convert(variables)
