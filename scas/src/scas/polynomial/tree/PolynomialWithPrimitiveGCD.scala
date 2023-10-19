bee887e16250c955156fd73144fc1b29013a9239
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-19 13:44:06 +0200

    empty

--- scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
@@ -2,18 +2,10 @@ package scas.polynomial.tree
 
 import scas.power.splittable.impl.PowerProduct
 import scas.structure.commutative.impl.UniqueFactorizationDomain
-import scas.polynomial.impl.MultivariatePolynomial
 import scas.polynomial.PolynomialOverUFD
-import scas.polynomial.impl.TreePolynomial
 import scas.polynomial.TreePolynomial.Element
-import scas.power.splittable.Lexicographic
-import scas.util.unary_~
 
-class PolynomialWithPrimitiveGCD[C : UniqueFactorizationDomain, M : PowerProduct] extends TreePolynomial[C, M] with MultivariatePolynomial[Element, C, M] with scas.polynomial.impl.PolynomialWithPrimitiveGCD[Element[C, M], C, M] with PolynomialOverUFD[Element[C, M], C, M] {
+class PolynomialWithPrimitiveGCD[C : UniqueFactorizationDomain, M : PowerProduct] extends MultivariatePolynomial[C, M] with scas.polynomial.impl.PolynomialWithPrimitiveGCD[Element[C, M], C, M] with PolynomialOverUFD[Element[C, M], C, M] {
   given instance: PolynomialWithPrimitiveGCD[C, M] = this
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[M]) => new PolynomialWithPrimitiveGCD(using ring, pp)
-}
-
-object PolynomialWithPrimitiveGCD {
-  def apply[C](ring: UniqueFactorizationDomain[C])(s: String*) = new PolynomialWithPrimitiveGCD(using ring, new Lexicographic[Int](s.map(~_): _*))
 }
--- scas/src/scas/polynomial/tree/PolynomialWithSubresGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithSubresGCD.scala
@@ -2,18 +2,10 @@ package scas.polynomial.tree
 
 import scas.power.splittable.impl.PowerProduct
 import scas.structure.commutative.impl.UniqueFactorizationDomain
-import scas.polynomial.impl.MultivariatePolynomial
 import scas.polynomial.PolynomialOverUFD
-import scas.polynomial.impl.TreePolynomial
 import scas.polynomial.TreePolynomial.Element
-import scas.power.splittable.Lexicographic
-import scas.util.unary_~
 
-class PolynomialWithSubresGCD[C : UniqueFactorizationDomain, M : PowerProduct] extends TreePolynomial[C, M] with MultivariatePolynomial[Element, C, M] with scas.polynomial.impl.PolynomialWithSubresGCD[Element[C, M], C, M] with PolynomialOverUFD[Element[C, M], C, M] {
+class PolynomialWithSubresGCD[C : UniqueFactorizationDomain, M : PowerProduct] extends MultivariatePolynomial[C, M] with scas.polynomial.impl.PolynomialWithSubresGCD[Element[C, M], C, M] with PolynomialOverUFD[Element[C, M], C, M] {
   given instance: PolynomialWithSubresGCD[C, M] = this
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[M]) => new PolynomialWithSubresGCD(using ring, pp)
-}
-
-object PolynomialWithSubresGCD {
-  def apply[C](ring: UniqueFactorizationDomain[C])(s: String*) = new PolynomialWithSubresGCD(using ring, new Lexicographic[Int](s.map(~_): _*))
 }
