b4a18fc9a5d57465ec49da3409699da7bac8cc06
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-19 21:57:03 +0200

    empty

--- scas/src/scas/polynomial/tree/UnivariatePolynomial.scala
+++ scas/src/scas/polynomial/tree/UnivariatePolynomial.scala
@@ -6,12 +6,11 @@ import scas.polynomial.PolynomialOverUFD
 import scas.polynomial.impl.TreePolynomial
 import scas.polynomial.TreePolynomial.Element
 import scas.power.Lexicographic
-import scas.util.unary_~
 
 class UnivariatePolynomial[C : Field, M : PowerProduct] extends TreePolynomial[C, M] with scas.polynomial.impl.UnivariatePolynomial[Element[C, M], C, M] with PolynomialOverUFD[Element[C, M], C, M] {
   given instance: UnivariatePolynomial[C, M] = this
 }
 
 object UnivariatePolynomial {
-  def apply[C](ring: Field[C])(s: String*) = new UnivariatePolynomial(using ring, new Lexicographic[Int](s.map(~_): _*))
+  def apply[C](ring: Field[C])(s: String*) = new UnivariatePolynomial(using ring, Lexicographic(0)(s: _*))
 }
