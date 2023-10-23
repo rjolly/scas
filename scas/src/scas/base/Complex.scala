ce47f48cfd77e0973d4bf58526422064e4825887
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-23 15:41:57 +0200

    empty

--- scas/src/scas/base/Complex.scala
+++ scas/src/scas/base/Complex.scala
@@ -1,7 +1,7 @@
 package scas.base
 
 import scas.polynomial.TreePolynomial.Element
-import scas.structure.commutative.impl.StarUFD
+import scas.structure.impl.StarRing
 import scas.residue.impl.TreeAlgebraicNumber
 import scas.residue.AlgebraicNumber
 import scas.power.Lexicographic
@@ -11,12 +11,13 @@ import Rational.given
 
 type Complex = Element[Rational, Array[Int]]
 
-object Complex extends Complex.Impl with AlgebraicNumber[Complex, Rational, Array[Int]] with scas.structure.commutative.StarUFD[Complex] {
+object Complex extends Complex.Impl with AlgebraicNumber[Complex, Rational, Array[Int]] with scas.structure.StarRing[Complex] {
   given instance: Complex.type = this
   abstract class Impl extends TreeAlgebraicNumber(using Rational, new Lexicographic[Int](Variable.sqrt(BigInteger("-1")))) with StarUFD[Complex] {
     import ring.pp
     def real(x: Complex) = ring(x.coefficient(pp.one))
     def imag(x: Complex) = ring(x.coefficient(pp.generator(0)))
+    def conjugate(x: Complex) = ???
     override def toString = "Complex"
     override def toMathML = "<complexes/>"
   }
