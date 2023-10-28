be8dccfcfebe0ad8e81c845ced36180717fe9593
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-28 15:07:32 +0200

    empty

--- scas/src/scas/structure/commutative/Quotient.scala
+++ scas/src/scas/structure/commutative/Quotient.scala
@@ -3,7 +3,7 @@ package scas.structure.commutative
 import scas.util.{Conversion, unary_~}
 import Quotient.Element
 
-trait Quotient[T : impl.UniqueFactorizationDomain] extends impl.Quotient[T] with Field[Element[T]] {
+trait Quotient[T] extends impl.Quotient[T] with Field[Element[T]] {
   given ring2quotient[U: Conversion[T]]: (U => Element[T]) = x => this(~x)
 }
 
