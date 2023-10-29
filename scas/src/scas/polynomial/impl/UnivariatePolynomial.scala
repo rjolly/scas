7d23b33f5270b38d9d5cb5fa3e263379fc303baf
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-29 17:51:43 +0100

    empty

--- scas/src/scas/polynomial/impl/UnivariatePolynomial.scala
+++ scas/src/scas/polynomial/impl/UnivariatePolynomial.scala
@@ -3,10 +3,12 @@ package scas.polynomial.impl
 import scala.annotation.tailrec
 import scas.structure.commutative.impl.{Field, EuclidianDomain}
 import scas.power.impl.PowerProduct
+import scas.base.BigInteger
 
-trait UnivariatePolynomial[T, C, M](using ring: Field[C], val pp: PowerProduct[M]) extends PolynomialOverField[T, C, M] with EuclidianDomain[T] {
+trait UnivariatePolynomial[T, C, M](using ring: Field[C], pp: PowerProduct[M]) extends PolynomialOverField[T, C, M] with EuclidianDomain[T] {
   assert (variables.length == 1)
   def derivative(x: T) = x.map((a, b) => (a / pp.generator(0), b * ring.fromInt(pp.degree(a))))
   override def gcd(x: T, y: T) = gcd1(x, y)
   @tailrec final def gcd1(x: T, y: T): T = if (y.isZero) x else gcd1(y, x.reduce(y))
+  extension (x: T) def coefficient(n: BigInteger) = x.coefficient(pp.generator(0) \ n)
 }
