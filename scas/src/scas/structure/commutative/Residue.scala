6fa9fdf2a612636244210b4d86238b45bc5ec0c2
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-29 10:08:50 +0100

    empty

--- scas/src/scas/structure/commutative/Residue.scala
+++
@@ -1,3 +0,0 @@
-package scas.structure.commutative
-
-trait Residue[T] extends impl.Residue[T] with UniqueFactorizationDomain[T]
--- scas/src/scas/structure/commutative/ordered/Residue.scala
+++
@@ -1,3 +0,0 @@
-package scas.structure.commutative.ordered
-
-trait Residue[T : impl.UniqueFactorizationDomain] extends impl.Residue[T] with scas.structure.commutative.Residue[T] with UniqueFactorizationDomain[T]
