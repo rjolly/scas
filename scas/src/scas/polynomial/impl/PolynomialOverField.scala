98b29f7b45876416915b9e5622c1dd58fdec1641
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-29 11:13:56 +0100

    empty

--- scas/src/scas/polynomial/impl/PolynomialOverField.scala
+++ scas/src/scas/polynomial/impl/PolynomialOverField.scala
@@ -1,14 +1,24 @@
 package scas.polynomial.impl
 
-import scala.annotation.tailrec
+import scala.reflect.ClassTag
 import scas.structure.commutative.impl.Field
+import scas.power.impl.PowerProduct
 
-trait PolynomialOverField[T, C : Field, M] extends PolynomialOverUFD[T, C, M] {
+trait PolynomialOverField[T : ClassTag, C : Field, M : PowerProduct] extends PolynomialOverUFD[T, C, M] {
   extension (x: T) override def %/ (c: C) = x%* Field[C].inverse(c)
   def monic(x: T) = if (x.isZero) zero else x%/ headCoefficient(x)
-  @tailrec final def gcd1(x: T, y: T): T = if (y.isZero) monic(x) else gcd1(y, monic(x.reduce(y)))
   extension (x: T) {
     override def reduce(m: M, a: C, y: T, b: C) = x.subtract(m, a / b, y)
     override def reduce(y: T) = super.reduce(x)(y)
+  }
+  extension (x: T) def modInverse(mod: T) = {
+    val s = new PolynomialWithRepr(using this)(1)
+    val (p, e) = s.gcd(s(x, 0), s(mod))
+    assert (p.isOne)
+    e(0)
+  }
+  extension (ring: Field[C]) def apply(s: T*) = {
+    assert (s == generators)
+    this
   }
 }
--- scas/src/scas/residue/Residue.scala
+++ scas/src/scas/residue/Residue.scala
@@ -1,8 +1,8 @@
 package scas.residue
 
-import scas.polynomial.impl.UnivariatePolynomial
+import scas.polynomial.impl.PolynomialOverField
 
-class Residue[T, C, M](using ring: UnivariatePolynomial[T, C, M])(mod: T) extends AlgebraicNumber[T, C, M] {
+class Residue[T, C, M](using ring: PolynomialOverField[T, C, M])(mod: T) extends AlgebraicNumber[T, C, M] {
   given instance: Residue[T, C, M] = this
   update(mod)
 }
