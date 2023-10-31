6b79ae1274a25ff37bed24cd93fa13882e48ea12
Raphael Jolly <rjolly@users.sourceforge.net>
2023-10-31 11:34:31 +0100

    empty

--- scas/src/scas/quotient/Quotient.scala
+++ scas/src/scas/quotient/Quotient.scala
@@ -2,6 +2,6 @@ package scas.quotient
 
 import scas.polynomial.impl.PolynomialOverField
 
-class Quotient[T, C, M](using PolynomialOverField[T, C, M]) extends RationalFunction[T, C, M] {
+class Quotient[T, C, M](using PolynomialOverField[T, C, M]) extends impl.RationalFunction[T, C, M] with scas.structure.commutative.Quotient[T] {
   given instance: Quotient[T, C, M] = this
 }
