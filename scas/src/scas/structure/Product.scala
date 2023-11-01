4820d9178d119c5f70a2470ccbaa2b2a213b9d4b
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-01 20:29:24 +0100

    empty

--- scas/src/scas/structure/Product.scala
+++ scas/src/scas/structure/Product.scala
@@ -1,13 +1,70 @@
 package scas.structure
 
 import scas.util.{Conversion, unary_~}
+import scas.base.BigInteger
+import BigInteger.lcm
+import impl.Ring
 
-class Product[R1 : impl.Ring, R2 : impl.Ring] extends impl.Product[R1, R2] with Ring[(R1, R2)] {
-  given instance: Product[R1, R2] = this
+class Product[R1, R2](using ring1: Ring[R1], ring2: Ring[R2]) extends Ring[(R1, R2)] {
+  def apply(a: R1, b: R2) = (a, b)
+  def fromInt(n: BigInteger) = (ring1.fromInt(n), ring2.fromInt(n))
+  extension (x: (R1, R2)) def add(y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    (a + c, b + d)
+  }
+  extension (x: (R1, R2)) def subtract(y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    (a - c, b - d)
+  }
+  extension (x: (R1, R2)) def multiply(y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    (a * c, b * d)
+  }
+  def equiv(x: (R1, R2), y: (R1, R2)) = {
+    val (a, b) = x
+    val (c, d) = y
+    a >< c && b >< d
+  }
+  extension (x: (R1, R2)) override def unary_- = {
+    val (a, b) = x
+    (-a, -b)
+  }
+  extension (a: (R1, R2)) override def pow(b: BigInteger) = {
+    val (c, d) = a
+    (c.pow(b), d.pow(b))
+  }
+  extension (x: (R1, R2)) def isUnit = {
+    val (a, b) = x
+    a.isUnit && b.isUnit
+  }
+  override def abs(x: (R1, R2)) = {
+    val (a, b) = x
+    (ring1.abs(a), ring2.abs(b))
+  }
+  extension (x: (R1, R2)) def signum = {
+    val (a, b) = x
+    if (a.signum == 0) b.signum else a.signum
+  }
+  def characteristic = lcm(ring1.characteristic, ring2.characteristic)
+  extension (x: (R1, R2)) def toCode(level: Level) = {
+    val (a, b) = x
+    s"Product(${a.toCode(Level.Addition)}, ${b.toCode(Level.Addition)})"
+  }
+  override def toString = s"Product($ring1, $ring2)"
+  extension (x: (R1, R2)) def toMathML = {
+    val (a, b) = x
+    s"<apply><cartesianproduct/>${a.toMathML}${b.toMathML}</apply>"
+  }
+  def toMathML = s"<apply><cartesianproduct/>${ring1.toMathML}${ring2.toMathML}</apply>"
+  def zero = (ring1.zero, ring2.zero)
+  def one = (ring1.one, ring2.one)
 }
 
 object Product {
-  def apply[R1, R2, U : Conversion[R1], V : Conversion[R2]](using factory: impl.Product[R1, R2])(a: U, b: V) = factory(~a, ~b)
+  def apply[R1, R2, U : Conversion[R1], V : Conversion[R2]](using factory: Product[R1, R2])(a: U, b: V) = factory(~a, ~b)
 
-  def apply[R1, R2](ring1: impl.Ring[R1], ring2: impl.Ring[R2]) = new Product(using ring1, ring2)
+  def apply[R1, R2](ring1: Ring[R1], ring2: Ring[R2]) = new conversion.Product(using ring1, ring2)
 }
