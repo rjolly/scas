bfd9f6577cb83dd9324066c4c5d07f86fc8b4d30
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-02 16:34:10 +0100

    empty

--- scas/src/scas/quotient/Quotient.scala
+++ scas/src/scas/quotient/Quotient.scala
@@ -1,20 +1,14 @@
 package scas.quotient
 
 import scas.polynomial.PolynomialOverField
-import scas.structure.commutative.Quotient.Element
 
 trait Quotient[T, C, M](using ring: PolynomialOverField[T, C, M]) extends scas.structure.commutative.Quotient[T] {
-  override def apply(x: Element[T]) = {
-    val Element(n, d) = x
-    val c = ring.gcd(n, d)
-    val gcd = c%/ ring.lastCoefficient(c)%* ring.lastCoefficient(d)
-    Element(n / gcd, d / gcd)
-  }
   def generator(n: Int) = this(ring.generator(n))
   def generators = ring.generators.map(apply)
   def toMathML = ring.toMathML(true)
+  export ring.coef2poly
 }
 
 object Quotient {
-  def apply[T, C, M](using ring: PolynomialOverField[T, C, M]) = new conversion.Quotient(using ring)
+  def apply[T, C, M](ring: PolynomialOverField[T, C, M]) = new conversion.Quotient(using ring)
 }
