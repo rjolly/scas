d0a7c1c30e82af613075de54d7409e65a15ba887
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-03 23:31:00 +0100

    empty

--- scas/src/scas/structure/Group.scala
+++ scas/src/scas/structure/Group.scala
@@ -7,19 +7,5 @@ trait Group[T] extends NotQuiteGroup[T] {
 }
 
 object Group {
-  trait FromAbelian[T](using group: AbelianGroup[T]) extends Group[T] {
-    override def one = group.zero
-    extension (x: T) {
-      def multiply(y: T) = group.add(x)(y)
-    }
-    def inverse(x: T) = group.unary_-(x)
-    def equiv(x: T, y: T) = group.equiv(x, y)
-    extension (x: T) {
-      def toCode(level: Level) = group.toCode(x)(level)
-      def toMathML = group.toMathML(x)
-    }
-    def toMathML = group.toMathML
-  }
-
-  def apply[T](group: AbelianGroup[T]) = new conversion.Group.FromAbelian(using group)
+  def apply[T](group: AbelianGroup[T]) = new scas.group.conversion.FromAbelian(using group)
 }
--- scas/src/scas/structure/conversion/Group.scala
+++ scas/src/scas/structure/conversion/Group.scala
@@ -1,11 +1,3 @@
 package scas.structure.conversion
 
-import scas.structure.AbelianGroup
-
 trait Group[T] extends scas.structure.Group[T] with NotQuiteGroup[T]
-
-object Group {
-  class FromAbelian[T](using group: AbelianGroup[T]) extends scas.structure.Group.FromAbelian[T] with Group[T] {
-    given instance: FromAbelian[T] = this
-  }
-}
