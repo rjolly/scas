2e818004dd8cf7b17d6676f27c319cbae819fd1e
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-04 18:14:15 +0100

    empty

--- scas/src/scas/base/conversion/ModInteger.scala
+++ scas/src/scas/base/conversion/ModInteger.scala
@@ -3,6 +3,6 @@ package scas.base.conversion
 import scas.structure.commutative.ordered.conversion.Field
 import scas.base.BigInteger
 
-class ModInteger(val mod: BigInteger) extends scas.base.ModInteger with Field[BigInteger] {
+class ModInteger(mod: BigInteger) extends scas.base.ModInteger(mod) with Field[BigInteger] {
   given instance: ModInteger = this
 }
