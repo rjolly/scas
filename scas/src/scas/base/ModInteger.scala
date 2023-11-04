dd470351a0ead571bd6ac867fd9b204431d5a40d
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-04 18:14:15 +0100

    empty

--- scas/src/scas/base/ModInteger.scala
+++ scas/src/scas/base/ModInteger.scala
@@ -4,9 +4,8 @@ import scas.structure.commutative.UniqueFactorizationDomain
 import scas.structure.commutative.ordered.{Field, Residue}
 import BigInteger.given
 
-abstract class ModInteger extends Residue[BigInteger] with Field[BigInteger] {
+class ModInteger(mod: BigInteger) extends Residue[BigInteger] with Field[BigInteger] {
   assert (mod.isProbablePrime(100))
-  def mod: BigInteger
   def apply(x: BigInteger) = x.mod(mod)
   def characteristic = mod
   extension (a: BigInteger) override def pow(b: BigInteger) = a.modPow(b, mod)
