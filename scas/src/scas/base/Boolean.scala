0a75f1f8c0a6c984088a741938abde2f455a50a4
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-12 16:41:47 +0100

    empty

--- scas/src/scas/base/Boolean.scala
+++ scas/src/scas/base/Boolean.scala
@@ -1,9 +1,26 @@
 package scas.base
 
 import scas.structure.BooleanRing
-import scas.structure.commutative.ordered.conversion.Field
+import scas.structure.commutative.Field
 
-object Boolean extends Boolean.Impl with Field[Int] with scas.structure.conversion.BooleanRing[Int] {
+object Boolean extends Boolean.Impl with scas.structure.commutative.conversion.Field[Boolean] with scas.structure.conversion.BooleanRing[Boolean] {
   given instance: Boolean.type = this
-  class Impl extends ModInteger(BigInteger("2")) with BooleanRing[Int]
+  abstract class Impl extends Field[Boolean] with BooleanRing[Boolean] {
+    def fromInt(n: BigInteger) = n.signum() != 0
+    extension (x: Boolean) {
+      def add(y: Boolean) = x ^ y
+      def subtract(y: Boolean) = x + y
+      def multiply(y: Boolean) = x && y
+    }
+    def inverse(x: Boolean) = x
+    def equiv(x: Boolean, y: Boolean) = x == y
+    extension (x: Boolean) def signum = if (x) 1 else 0
+    extension (x: Boolean) def toCode(level: Level) = x.toString
+    override def toString = "Boolean"
+    extension (x: Boolean) def toMathML = if (x) "<true/>" else "<false/>"
+    def toMathML = "<ci>Boolean</ci>"
+  }
+  val characteristic = BigInteger("2")
+  val zero = false
+  val one = true
 }
