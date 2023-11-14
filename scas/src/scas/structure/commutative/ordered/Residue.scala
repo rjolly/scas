6bfdce5bb9376ea0d7aa192c8d29eb9e0f933105
Raphael Jolly <rjolly@users.sourceforge.net>
2023-11-14 11:23:03 +0100

    empty

--- scas/src/scas/structure/commutative/ordered/Residue.scala
+++ scas/src/scas/structure/commutative/ordered/Residue.scala
@@ -2,10 +2,10 @@ package scas.structure.commutative.ordered
 
 trait Residue[T, R](using ring: UniqueFactorizationDomain[R]) extends scas.structure.commutative.Residue[T, R] with UniqueFactorizationDomain[T] {
   def compare(x: T, y: T) = {
-    val this(a) = x: @unchecked
-    val this(b) = y: @unchecked
-    val this(c) = this(a): @unchecked
-    val this(d) = this(b): @unchecked
+    val a = x.unapply
+    val b = y.unapply
+    val c = this(a).unapply
+    val d = this(b).unapply
     ring.compare(c, d)
   }
   extension (x: T) override def signum = super.signum(x)
