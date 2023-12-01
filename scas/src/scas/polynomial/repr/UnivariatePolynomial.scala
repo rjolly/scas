d9ca56548bb741a10e7187efb7518b01a7f91469
Raphael Jolly <rjolly@users.sourceforge.net>
2023-12-01 11:33:49 +0100

    empty

---
+++ scas/src/scas/polynomial/repr/UnivariatePolynomial.scala
@@ -0,0 +1,9 @@
+package scas.polynomial.repr
+
+import scala.reflect.ClassTag
+import scas.polynomial.{Polynomial, PolynomialWithRepr}
+import scas.structure.commutative.Field
+import scas.power.PowerProduct
+import PolynomialWithRepr.Element
+
+class UnivariatePolynomial[T : ClassTag, C : Field, M : PowerProduct](using Polynomial[T, C, M])(dimension: Int) extends PolynomialWithRepr[T, C, M](dimension) with scas.polynomial.UnivariatePolynomial[Element[T], C, M]
