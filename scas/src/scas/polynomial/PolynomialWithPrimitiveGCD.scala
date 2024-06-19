64a881bd7f4c442ca8f120113d12e1d005f7bc13
Raphael Jolly <rjolly@users.sourceforge.net>
2024-06-19 13:17:58 +0200

    empty

--- scas/src/scas/polynomial/PolynomialWithPrimitiveGCD.scala
+++ scas/src/scas/polynomial/PolynomialWithPrimitiveGCD.scala
@@ -2,6 +2,6 @@ package scas.polynomial
 
 import scala.annotation.tailrec
 
-trait PolynomialWithPrimitiveGCD[T, C, M] extends PolynomialOverUFD[T, C, M] {
-  @tailrec final def gcd1(x: T, y: T): T = if (y.isZero) primitivePart(x) else gcd1(y, primitivePart(x.reduce(y)))
+trait PolynomialWithPrimitiveGCD[T[C, M], C, M] extends MultivariatePolynomial[T, C, M] {
+  @tailrec final def gcd1(x: T[C, M], y: T[C, M]): T[C, M] = if (y.isZero) primitivePart(x) else gcd1(y, primitivePart(x.reduce(y)))
 }
--- scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
@@ -4,7 +4,7 @@ import scas.power.PowerProduct
 import scas.structure.commutative.UniqueFactorizationDomain
 import scas.polynomial.TreePolynomial.Element
 
-class PolynomialWithPrimitiveGCD[C : UniqueFactorizationDomain, M : PowerProduct] extends MultivariatePolynomial[C, M] with scas.polynomial.PolynomialWithPrimitiveGCD[Element[C, M], C, M] {
+class PolynomialWithPrimitiveGCD[C : UniqueFactorizationDomain, M : PowerProduct] extends MultivariatePolynomial[C, M] with scas.polynomial.PolynomialWithPrimitiveGCD[Element, C, M] {
   given instance: MultivariatePolynomial[C, M] = this
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[M]) => new PolynomialWithPrimitiveGCD(using ring, pp)
 }
