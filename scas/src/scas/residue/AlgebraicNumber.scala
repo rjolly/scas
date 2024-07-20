fd95d3e4e01459a06b25476c9f1ad6197e6fcadd
Raphael Jolly <rjolly@users.sourceforge.net>
2024-07-20 14:38:40 +0200

    empty

--- scas/src/scas/residue/AlgebraicNumber.scala
+++ scas/src/scas/residue/AlgebraicNumber.scala
@@ -3,7 +3,9 @@ package scas.residue
 import scas.structure.commutative.Field
 import scas.variable.Variable
 import scas.util.Conversion
+import scas.base.ModInteger
 
 object AlgebraicNumber {
   def apply[C, S : Conversion[Variable]](ring: Field[C])(s: S*) = new conversion.AlgebraicNumber(using ring)(s*)
+  def galoisField[S : Conversion[Variable]](str: String)(s: S*) = this(ModInteger(str))(s*)
 }
--- scas/src/scas/residue/GaloisField.scala
+++
@@ -1,8 +0,0 @@
-package scas.residue
-
-import scas.variable.Variable
-import scas.util.Conversion
-
-object GaloisField {
-  def apply[S : Conversion[Variable]](str: String)(s: S*) = new conversion.GaloisField(str)(s*)
-}
