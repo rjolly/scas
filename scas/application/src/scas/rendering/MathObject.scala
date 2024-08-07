8874da9a217142da37fc6bf49933359dd1f44e46
Raphael Jolly <rjolly@users.sourceforge.net>
2024-08-07 14:30:45 +0200

    empty

--- scas/application/src/scas/rendering/MathObject.scala
+++ scas/application/src/scas/rendering/MathObject.scala
@@ -1,16 +1,22 @@
 package scas.rendering
 
-import jscl.editor.rendering.MathObject
 import scas.util.{Conversion, unary_~}
+import scas.structure.Structure
 import scas.prettyprint.Show
+
+type MathObject = jscl.editor.rendering.MathObject
 
 object MathObject {
   def apply[U : Conversion[T], T : Show](x: U): MathObject = new MathObject {
     override def toString = (~x).show
     def toMathML = (~x).toMathML
   }
-  def apply[T : Show](s: List[T]): MathObject = new MathObject {
-    override def toString = s.show
-    def toMathML = s.toMathML
+  def apply[U : Conversion[T], T : Show](s: List[U]): MathObject = new MathObject {
+    override def toString = s.map(~_).show
+    def toMathML = s.map(~_).toMathML
+  }
+  def apply[T](x: Structure[T]): MathObject = new MathObject {
+    override def toString = x.toString
+    def toMathML = x.toMathML
   }
 }
