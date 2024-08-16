74ada7de2c5780d9c225e0967c740d2c279ae66e
Raphael Jolly <rjolly@users.sourceforge.net>
2024-08-16 11:39:55 +0200

    empty

--- scas/src/scas/variable/Function.scala
+++ scas/src/scas/variable/Function.scala
@@ -3,6 +3,6 @@ package scas.variable
 import scas.prettyprint.Show
 
 class Function[T : Show](name: String, parameter: T*) extends Variable {
-  override def toString = s"$name(${parameter.toList.show(false)})"
-  def toMathML = s"<apply><ci>${name.toMathML}</ci>${parameter.toList.toMathML(false)}</apply>"
+  override val toString = s"$name(${parameter.toList.show(false)})"
+  val toMathML = s"<apply><ci>${name.toMathML}</ci>${parameter.toList.toMathML(false)}</apply>"
 }
--- scas/src/scas/variable/Sqrt.scala
+++ scas/src/scas/variable/Sqrt.scala
@@ -3,5 +3,5 @@ package scas.variable
 import scas.prettyprint.Show
 
 class Sqrt[T : Show](x: T) extends Function("sqrt", x) {
-  override def toMathML = s"<apply><root/>${x.toMathML}</apply>"
+  override val toMathML = s"<apply><root/>${x.toMathML}</apply>"
 }
