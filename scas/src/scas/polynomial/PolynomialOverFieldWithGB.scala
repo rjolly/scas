f8090118d8b8871ed300e7e6360e06d61ea7fce3
Raphael Jolly <rjolly@users.sourceforge.net>
2024-08-17 11:11:48 +0200

    empty

--- scas/src/scas/polynomial/PolynomialOverFieldWithGB.scala
+++ scas/src/scas/polynomial/PolynomialOverFieldWithGB.scala
@@ -4,10 +4,13 @@ import scala.reflect.ClassTag
 import scas.math.Numeric
 import scas.power.ArrayPowerProduct
 import scas.module.Array
+import scas.variable.Variable
 import scas.base.BigInteger
 import BigInteger.given
 
 trait PolynomialOverFieldWithGB[T : ClassTag, C, N : Numeric : ClassTag] extends PolynomialWithGB[T, C, N] with PolynomialOverField[T, C, Array[N]] {
+  def newInstance(pp: ArrayPowerProduct[N]): PolynomialOverFieldWithGB[T, C, N]
+  def newInstance(s: Variable*): PolynomialOverFieldWithGB[T, C, N] = newInstance(pp.newInstance(s*))
   extension (x: T) def modInverse(mods: T*) = {
     given module: Module[T, C, N] = new Module(using this)("c", 2)
     val s = Seq(Array(x, 1)) ++ mods.map(Array(_, 0))
--- scas/src/scas/residue/AlgebraicNumber.scala
+++ scas/src/scas/residue/AlgebraicNumber.scala
@@ -2,16 +2,16 @@ package scas.residue
 
 import scas.power.DegreeReverseLexicographic
 import scas.polynomial.TreePolynomial.Element
-import scas.polynomial.PolynomialOverField
+import scas.polynomial.tree.PolynomialOverField
+import scas.polynomial.PolynomialOverFieldWithGB
 import scas.structure.commutative.Field
-import AlgebraicNumber.newInstance
 import scas.variable.Variable
 import scas.util.Conversion
 
-class AlgebraicNumber[C](using var ring: PolynomialOverField[Element[C, Array[Int]], C, Array[Int]]) extends Residue[Element[C, Array[Int]], C, Array[Int]] {
-  def this(ring: Field[C])(s: Variable*) = this(using newInstance(ring)(s*))
+class AlgebraicNumber[C](using var ring: PolynomialOverFieldWithGB[Element[C, Array[Int]], C, Int]) extends Residue[Element[C, Array[Int]], C, Array[Int]] {
+  def this(ring: Field[C])(s: Variable*) = this(using new PolynomialOverField(using ring, DegreeReverseLexicographic(0)(s*)))
   def extend(s: Variable*): Unit = {
-    ring = newInstance(ring.ring)((ring.variables ++ s)*)
+    ring = ring.newInstance((ring.variables ++ s)*)
     mods = mods.map(_.convert)
   }
   extension (x: Element[C, Array[Int]]) def convert = ring.convert(x)
@@ -19,5 +19,4 @@ class AlgebraicNumber[C](using var ring: PolynomialOverField[Element[C, Array[In
 
 object AlgebraicNumber {
   def apply[C, S : Conversion[Variable]](ring: Field[C])(s: S*) = new conversion.AlgebraicNumber(ring)(s*)
-  def newInstance[C](ring: Field[C])(s: Variable*) = new scas.polynomial.tree.PolynomialOverField(using ring, DegreeReverseLexicographic(0)(s*))
 }
