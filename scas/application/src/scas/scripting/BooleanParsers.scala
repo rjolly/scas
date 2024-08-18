5ea88281146ac1ae0cbfc30cd3c671cddb72e7c5
Raphael Jolly <rjolly@users.sourceforge.net>
2024-08-18 14:17:17 +0200

    empty

--- scas/application/src/scas/scripting/BooleanParsers.scala
+++ scas/application/src/scas/scripting/BooleanParsers.scala
@@ -1,28 +1,10 @@
 package scas.scripting
 
 import Parsers._
-import scala.annotation.nowarn
 import scas.base.Boolean
 
-class BooleanParsers(val rf: RFParsers) extends UFDParsers[Boolean] {
+object BooleanParsers extends BooleanRingParsers[Boolean] {
   given structure: Boolean.Impl = Boolean
   def boolean: Parser[Boolean] = ("true" | "false") ^^ { _.toBoolean }
-  @nowarn("msg=match may not be exhaustive")
-  override def comparison: Parser[Boolean] = base ~ ("=>" | "=" | "<>") ~ base ^^ {
-    case x ~ "=>" ~ y => y || !x
-    case x ~ "=" ~ y => x == y
-    case x ~ "<>" ~ y => x != y
-  }
-  def negation: Parser[Boolean] = "!" ~> base ^^ { case x => !x }
-  def function: Parser[Boolean] = RationalParsers.comparison | rf.comparison | DoubleParsers.comparison | ComplexParsers.comparison | comparison | negation
   def base: Parser[Boolean] = boolean | "(" ~> expr <~ ")"
-  override def term: Parser[Boolean] = function | base
-  @nowarn("msg=match may not be exhaustive")
-  override def expr: Parser[Boolean] = term ~ rep(("&&" | "||" | "^") ~ term) ^^ {
-    case term ~ list => list.foldLeft(term) {
-      case (x, "&&" ~ y) => x && y
-      case (x, "||" ~ y) => x || y
-      case (x, "^" ~ y) => x ^ y
-    }
-  }
 }
