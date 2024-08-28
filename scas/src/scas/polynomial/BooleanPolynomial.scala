cde986e55029a63c973ff7a25efaefc9577650b0
Raphael Jolly <rjolly@users.sourceforge.net>
2024-08-28 10:27:56 +0200

    empty

--- scas/src/scas/polynomial/BooleanPolynomial.scala
+++
@@ -1,7 +0,0 @@
-package scas.polynomial
-
-import scas.base.Boolean
-
-trait BooleanPolynomial[T[Boolean, M], M] extends PolynomialWithSubresGCD[T, Boolean, M] {
-  given ring: Boolean.type = Boolean
-}
--- scas/src/scas/residue/BooleanAlgebra.scala
+++ scas/src/scas/residue/BooleanAlgebra.scala
@@ -2,15 +2,15 @@ package scas.residue
 
 import scas.structure.BooleanRing
 import scas.polynomial.TreePolynomial.Element
-import scas.polynomial.tree.BooleanPolynomial
+import scas.polynomial.tree.PolynomialWithSubresGCD
 import scas.polynomial.PolynomialOverUFD
 import scas.variable.Variable
 import scas.util.Conversion
-import scas.base.BigInteger
+import scas.base.{BigInteger, Boolean}
 import BigInteger.given
 
 class BooleanAlgebra(using val ring: PolynomialOverUFD[Element[Boolean, Array[Int]], Boolean, Array[Int]]) extends Residue[Element[Boolean, Array[Int]], Boolean, Array[Int]] with BooleanRing[Element[Boolean, Array[Int]]] {
-  def this(s: Variable*) = this(using new BooleanPolynomial(s*))
+  def this(s: Variable*) = this(using new PolynomialWithSubresGCD(using Boolean)(s*))
   for (x <- generators) {
     update(x+x\2)
   }
