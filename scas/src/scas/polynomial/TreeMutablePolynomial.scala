6401b6854bdee9475cad680f6a41183a42e6c85f
Raphael Jolly <rjolly@users.sourceforge.net>
2024-09-10 16:59:24 +0200

    empty

--- scas/src/scas/polynomial/TreeMutablePolynomial.scala
+++ scas/src/scas/polynomial/TreeMutablePolynomial.scala
@@ -4,42 +4,44 @@ import scala.jdk.CollectionConverters.MapHasAsScala
 import TreePolynomial.Element
 
 abstract class TreeMutablePolynomial[C, M] extends TreePolynomial[C, M] {
-  extension (x: Element[C, M]) override def subtract(y: Element[C, M]) = unmodifiable(super.subtract(modifiable(x))(y))
+  extension (x: Element[C, M]) {
+    override def subtract(y: Element[C, M]) = unmodifiable(super.subtract(modifiable(x))(y))
 
-  extension (x: Element[C, M]) override def multiply(y: Element[C, M]) = {
-    val r = modifiable(zero)
-    for ((a, b) <- y.asScala) r.subtract(a, -b, x)
-    unmodifiable(r)
-  }
+    override def multiply(y: Element[C, M]) = {
+      val r = modifiable(zero)
+      for ((a, b) <- y.asScala) r.subtract(a, -b, x)
+      unmodifiable(r)
+    }
 
-  extension (x: Element[C, M]) override def reduce(tail: Boolean, ys: Element[C, M]*) = unmodifiable(super.reduce(modifiable(x))(tail, ys*))
+    override def reduce(tail: Boolean, ys: Element[C, M]*) = unmodifiable(super.reduce(modifiable(x))(tail, ys*))
 
-  extension (x: Element[C, M]) override def subtract(m: M, c: C, y: Element[C, M]) = {
-    val ys = y.entrySet.iterator
-    while (ys.hasNext) {
-      val sa = ys.next
-      val s = sa.getKey
-      val a = sa.getValue
-      val ac = a * c
-      if (!ac.isZero) {
-        val sm = s * m
-        val cc = x.getOrElse(sm, ring.zero) - ac
-        if (cc.isZero) x.remove(sm) else x.put(sm, cc)
+    override def subtract(m: M, c: C, y: Element[C, M]) = {
+      val ys = y.entrySet.iterator
+      while (ys.hasNext) {
+        val sa = ys.next
+        val s = sa.getKey
+        val a = sa.getValue
+        val ac = a * c
+        if (!ac.isZero) {
+          val sm = s * m
+          val cc = x.getOrElse(sm, ring.zero) - ac
+          if (cc.isZero) x.remove(sm) else x.put(sm, cc)
+        }
       }
+      x
     }
-    x
-  }
 
-  extension (x: Element[C, M]) override def multiplyRight(c: C) = {
-    val xs = x.entrySet.iterator
-    while (xs.hasNext) {
-      val sa = xs.next
-      val s = sa.getKey
-      val a = sa.getValue
-      val ac = a * c
-      if (!ac.isZero) sa.setValue(ac)
-      else xs.remove
+    override def multiplyRight(c: C) = {
+      val xs = x.entrySet.iterator
+      while (xs.hasNext) {
+        val sa = xs.next
+        val s = sa.getKey
+        val a = sa.getValue
+        val ac = a * c
+        if (!ac.isZero) sa.setValue(ac)
+        else xs.remove
+      }
+      x
     }
-    x
   }
 }
