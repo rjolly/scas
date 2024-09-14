7aa400e1f4fcf1b5bb306e992ba9ebd3f58b5e09
Raphael Jolly <rjolly@users.sourceforge.net>
2024-09-14 15:03:36 +0200

    empty

--- scas/application/src/scas/scripting/Int.scala
+++ scas/application/src/scas/scripting/Int.scala
@@ -2,7 +2,6 @@ package scas.scripting
 
 import Parsers._
 import scala.annotation.{nowarn, tailrec}
-import scala.collection.immutable.{Map, SortedMap}
 import scas.base.BigInteger
 import BigInteger.int2bigInt
 
@@ -23,24 +22,4 @@ object Int extends OrderedRingParsers[BigInteger] {
   def factorial(x: BigInteger): BigInteger = factorial(BigInteger("1"), x)
 
   @tailrec final def factorial(res: BigInteger, x: BigInteger): BigInteger = if (x > 1) factorial(x * res, x - 1) else res
-
-  def factor(x: BigInteger): Map[BigInteger, Int] = {
-    assert(x > 0)
-    factor(x, SortedMap.empty[BigInteger, Int], primes)
-  }
-
-  @tailrec final def factor(x: BigInteger, map: Map[BigInteger, Int], primes: LazyList[BigInteger]): Map[BigInteger, Int] = {
-    val y = primes.head
-    if (x >< 1) map
-    else if (y * y > x) map + ((x, map.getOrElse(x, 0) + 1))
-    else if (y | x) factor(x / y, map + ((y, map.getOrElse(y, 0) + 1)), primes)
-    else factor(x, map, primes.tail)
-  }
-
-  def sieve(n: BigInteger): LazyList[BigInteger] = {
-    if (primes.takeWhile(p => p * p <= n).exists(_ | n)) sieve(n + 2)
-    else n #:: sieve(n + 2)
-  }
-
-  val primes: LazyList[BigInteger] = BigInteger("2") #:: sieve(BigInteger("3"))
 }
--- scas/application/src/scas/scripting/NormalForm.scala
+++ scas/application/src/scas/scripting/NormalForm.scala
@@ -3,5 +3,6 @@ package scas.scripting
 import scas.residue.BooleanAlgebra
 
 class NormalForm(using val ring: BooleanAlgebra) extends Factors[BA, Int] {
+  def empty = Map.empty[BA, Int]
   def factor(x: BA) = ring.gb(!x).foldLeft(one)((l, a) => l * this(!a))
 }
