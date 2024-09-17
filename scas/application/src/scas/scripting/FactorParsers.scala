48a0bab26a4ac45e5d605b645d81726400d99ac6
Raphael Jolly <rjolly@users.sourceforge.net>
2024-09-17 22:45:27 +0200

    empty

--- scas/application/src/scas/scripting/FactorParsers.scala
+++ scas/application/src/scas/scripting/FactorParsers.scala
@@ -5,6 +5,6 @@ import Parsers._
 object FactorParsers extends StructureParsers[FS] {
   given structure: FS.type = FS
   def expr: Parser[FS] = ("factor") ~ ("(" ~> Int.expr) <~ ")" ^^ {
-    case "factor" ~ x => structure.factor(x)
+    case "factor" ~ x => structure(x)
   }
 }
