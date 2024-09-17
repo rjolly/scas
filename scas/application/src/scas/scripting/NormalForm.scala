4ae97907666e0819f75f6ee155788086854502e9
Raphael Jolly <rjolly@users.sourceforge.net>
2024-09-17 16:07:29 +0200

    empty

--- scas/application/src/scas/scripting/NormalForm.scala
+++ scas/application/src/scas/scripting/NormalForm.scala
@@ -8,15 +8,16 @@ class NormalForm(conj: Boolean)(using val ring: BooleanAlgebra) extends Factors[
   override def apply(x: BA) = if (x.isZero) zero else ring.gb(if (conj) !x else x).foldLeft(one)((l, a) => l * super.apply(if (conj) !a else a))
   extension (x: Element[BA, Int]) {
     override def toCode(level: Level) = {
+      val p = if (x.size == 1) level else if(conj) Level.Multiplication else Level.Addition
       var s = ring.one.show
       var m = 0
       for ((a, b) <- x) {
-        val t = if (!a.isNot || !conj) a.show else s"!${(!a).toCode(Level.Multiplication)}"
+        val t = if (!a.isNot || !conj) a.toCode(p) else s"!${(!a).toCode(Level.Power)}"
         val times = if(conj) " && " else " || "
         s = if (m == 0) t else s + times + t
         m += 1
       }
-      s
+      if (level > p) fenced(s) else s
     }
     override def toMathML = {
       var s = ring.one.toMathML
