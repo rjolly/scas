b75a6776a05644685720593d9cf20995d78571ce
Raphael Jolly <rjolly@users.sourceforge.net>
2024-09-29 12:17:20 +0200

    empty

--- scas/application/src/scas/scripting/BooleanAlgebra.scala
+++ scas/application/src/scas/scripting/BooleanAlgebra.scala
@@ -5,7 +5,7 @@ import scas.variable.Variable
 class BooleanAlgebra(on: Boolean, recurse: Boolean, conj: Boolean, s: Variable*) extends BooleanAlgebraWithNot(s*) {
   def this(conj: Boolean, s: Variable*) = this(true, true, conj, s*)
   def this(s: Variable*) = this(true, s*)
-  given nf: NormalForm = new NormalForm(conj)(using new BooleanAlgebra(recurse, false, !conj, s*))
+  given nf: NormalForm = new NormalFormWithImpl(conj)(using new BooleanAlgebra(recurse, false, !conj, s*))
   extension (x: BA) {
     override def toCode(level: Level) = if (on) nf(x).toCode(level) else super.toCode(x)(level)
     override def toMathML = if (on) nf(x).toMathML else super.toMathML(x)
