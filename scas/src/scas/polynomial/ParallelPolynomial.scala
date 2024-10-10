722ae0b6ac29a91020446de9701cd247ba688bdf
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-10 18:31:57 +0200

    empty

---
+++ scas/src/scas/polynomial/ParallelPolynomial.scala
@@ -0,0 +1,10 @@
+package scas.polynomial
+
+import scala.collection.parallel.CollectionConverters._
+
+trait ParallelPolynomial[T, C, M] extends Polynomial[T, C, M] {
+  extension (x: T) override def multiply(y: T) = y.toSeq.par.aggregate(zero)({ (l, r) =>
+    val (a, b) = r
+    l.subtract(a, -b, x)
+  }, _ + _)
+}
