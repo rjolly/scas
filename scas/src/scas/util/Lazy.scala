f526bfa9e4ce461bdb0cd6176c7cc754b385c0b9
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-18 21:39:57 +0200

    empty

--- scas/src/scas/util/Lazy.scala
+++ scas/src/scas/util/Lazy.scala
@@ -4,20 +4,15 @@ import scala.concurrent.{ExecutionContext, CanAwait, Future}
 import scala.concurrent.duration.Duration
 import scala.util.{Try, Success}
 
-trait Lazy[+T] extends Future[T] with (() => T) {
-  def value = Some(Success(apply))
+class Lazy[+T](body: => T) extends Future[T] {
+  lazy val await = body
+  def value = Some(Success(await))
   def isCompleted = true
   def onComplete[U](func: Try[T] => U)(implicit executor: ExecutionContext) = ()
   def ready(atMost: Duration)(implicit permit: CanAwait) = this
-  def result(atMost: Duration)(implicit permit: CanAwait) = apply
-  override def map[S](f: T => S)(implicit executor: ExecutionContext) = Lazy(f(apply))
-  override def flatMap[S](f: T => Future[S])(implicit executor: ExecutionContext) = f(apply)
+  def result(atMost: Duration)(implicit permit: CanAwait) = await
+  override def map[S](f: T => S)(implicit executor: ExecutionContext) = Lazy(f(await))
+  override def flatMap[S](f: T => Future[S])(implicit executor: ExecutionContext) = f(await)
   def transform[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext): Future[S] = ???
   def transformWith[S](f: Try[T] => Future[S])(implicit executor: ExecutionContext): Future[S] = ???
-}
-
-object Lazy {
-  def apply[T](body: => T) = new Lazy[T] {
-    lazy val apply = body
-  }
 }
