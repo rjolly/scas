71eaf4b5bfb05cb1e570d456aec7b5dded50e727
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-19 18:38:15 +0200

    empty

--- scas/src/scas/util/Stream.scala
+++ scas/src/scas/util/Stream.scala
@@ -34,6 +34,8 @@ object Stream {
 
   def apply[A](s: A*): Stream[A] = if (!s.isEmpty) s.head #: Future(apply(s.tail*)) else Nil
 
+  def sequential[A](s: A*): Stream[A] = if (!s.isEmpty) s.head #: Lazy(sequential(s.tail*)) else Nil
+
   private class Iterator[+A](private var stream: Stream[A]) extends scala.collection.Iterator[A] {
     override def hasNext: Boolean = !stream.isEmpty
 
