773f08ce31d4407e69db5f64ede0bdb06582152c
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-19 16:15:17 +0200

    empty

---
+++ scas/src/scas/polynomial/ListPolynomial.scala
@@ -0,0 +1,63 @@
+package scas.polynomial
+
+import scala.collection.mutable.ListBuffer
+import ListPolynomial.Element
+
+trait ListPolynomial[C, M] extends Polynomial[Element[C, M], C, M] {
+  def apply(s: (M, C)*) = List(s*)
+
+  extension (x: Element[C, M]) {
+    def add(y: Element[C, M]) = {
+      val res = new ListBuffer[(M, C)]
+      var leftx = x
+      var lefty = y
+      while (!leftx.isEmpty && !lefty.isEmpty) {
+        val (s, a) = leftx.head
+        val (t, b) = lefty.head
+        if (s > t) {
+          res += leftx.head
+          leftx = leftx.tail
+        } else if (s < t) {
+          res += lefty.head
+          lefty = lefty.tail
+        } else {
+          val c = a + b
+          if (!c.isZero) res += ((s, c))
+          leftx = leftx.tail
+          lefty = lefty.tail
+        }
+      }
+      res ++= leftx
+      res ++= lefty
+      res.toList
+    }
+
+    override def isZero = x.isEmpty
+
+    def iterator = x.iterator
+
+    override def toSeq = x
+
+    def size = x.size
+
+    def head = x.head
+
+    def last = x.last
+
+    def map(f: (M, C) => (M, C)) = {
+      val res = new ListBuffer[(M, C)]
+      var left = x
+      while (!left.isEmpty) {
+        val (s, a) = left.head
+        val (m, c) = f(s, a)
+        if (!c.isZero) res += ((m, c))
+        left = left.tail
+      }
+      res.toList
+    }
+  }
+}
+
+object ListPolynomial {
+  type Element[C, M] = List[(M, C)]
+}
