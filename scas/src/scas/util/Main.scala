4cc2d2276ff355d530c2481e10727b395649a9ea
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-19 18:37:51 +0200

    empty

--- scas/src/scas/util/Main.scala
+++ scas/src/scas/util/Main.scala
@@ -1,8 +1,16 @@
 package scas.util
 
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+
 extension [U](x: U)
   def unary_~[T](using c: U => T) = c(x)
 
 type Conversion[T] = [X] =>> X => T
 
 type ToFrags[T] = [X] =>> Fragable[X, T]
+
+given ExecutionContext = ExecutionContext.global
+
+extension[T](s: Future[T])
+  def await = Await.result(s, Duration.Inf)
