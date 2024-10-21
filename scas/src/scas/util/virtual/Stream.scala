523c507c24f6eebe3171b44bd4622275d0fb4b54
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-21 16:48:32 +0200

    empty

--- scas/src/scas/util/virtual/Stream.scala
+++ scas/src/scas/util/virtual/Stream.scala
@@ -2,8 +2,9 @@ package scas.util.virtual
 
 import scala.concurrent.Future
 import scas.util.{await, given}
+import Stream.{#:, Nil}
 
-trait Stream[+A] {
+sealed trait Stream[+A] {
   def isEmpty: Boolean
   def head: A
   def tail: Stream[A]
@@ -16,31 +17,34 @@ trait Stream[+A] {
   def filter(p: A => Boolean): Stream[A] = {
     var rest = this
     while (!rest.isEmpty && !p(rest.head)) rest = rest.tail
-    if (!rest.isEmpty) {
-      rest.head #: rest.tail.filter(p)
-    } else Stream.Nil
+    rest match {
+      case head #: tail => head #: tail.await.filter(p)
+      case Nil => Nil
+      case _ => ???
+    }
   }
 
   def iterator: Iterator[A] = new Stream.Iterator(this)
 }
 
 object Stream {
-  object Nil extends Stream[Nothing] {
+  case object Nil extends Stream[Nothing] {
     def isEmpty = true
     def head = ???
     def tail = ???
   }
 
-  final class Cons[+A](hd: A, tl: Future[Stream[A]]) extends Stream[A] {
+  case class Cons[+A](hd: A, tl: Future[Stream[A]]) extends Stream[A] {
     def this(hd: A, tail: => Stream[A]) = this(hd, Future(tail))
     def isEmpty = false
     def head = hd
     def tail = tl.await
   }
 
+  val #: = Cons
   extension [A](x: A)
     def #:(xs1: => Stream[A]): Stream[A] =
-      Cons(x, xs1)
+      new Cons(x, xs1)
 
   def apply[A](s: A*): Stream[A] = if (!s.isEmpty) s.head #: apply(s.tail*) else Nil
 
@@ -51,7 +55,7 @@ object Stream {
   }
 
   object sequential {
-    final class Cons[+A](hd: A, tl: => Stream[A]) extends Stream[A] {
+    class Cons[+A](hd: A, tl: => Stream[A]) extends Stream[A] {
       def isEmpty = false
       def head = hd
       lazy val tail = tl
