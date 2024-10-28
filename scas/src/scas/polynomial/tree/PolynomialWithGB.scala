07308edbcadd7a1e2d93a0eea2703aa55e704971
Raphael Jolly <rjolly@users.sourceforge.net>
2024-10-28 14:56:20 +0100

    empty

--- scas/src/scas/polynomial/tree/PolynomialWithGB.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithGB.scala
@@ -9,8 +9,5 @@ import TreePolynomial.Element
 
 class PolynomialWithGB[C, N](using val ring: UniqueFactorizationDomain[C], val pp: ArrayPowerProduct[N])(using ClassTag[N], Numeric[N]) extends TreePolynomial[C, Array[N]] with scas.polynomial.ufd.PolynomialWithGB[Element[C, Array[N]], C, N] with UniqueFactorizationDomain.Conv[Element[C, Array[N]]] {
   given instance: PolynomialWithGB[C, N] = this
-  object Implicits {
-    export PolynomialWithGB.this.{instance, coef2poly}
-  }
   def newInstance(pp: ArrayPowerProduct[N]) = new PolynomialWithGB(using ring, pp)
 }
