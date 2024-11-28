b228fb210ab5b057fe255807dd9aa55ac3945f30
Raphael Jolly <rjolly@users.sourceforge.net>
2024-11-28 09:33:21 +0100

    empty

--- scas/src/scas/power/ArrayPowerProduct.scala
+++ scas/src/scas/power/ArrayPowerProduct.scala
@@ -3,7 +3,8 @@ package scas.power
 import scas.math.Numeric
 import scas.variable.Variable
 
-abstract class ArrayPowerProduct[N](using numeric: Numeric[N]) extends PowerProduct[Array[N]] {
+trait ArrayPowerProduct[N](using numeric: Numeric[N]) extends PowerProduct[Array[N]] {
+  def empty: Array[N]
   def newInstance(variables: Variable*): ArrayPowerProduct[N]
   extension (x: Array[N]) def dependencyOnVariables = (for (i <- 0 until length if (x(i) > numeric.zero)) yield i).toArray
   extension (x: Array[N]) def toCode(level: Level, times: String) = {
