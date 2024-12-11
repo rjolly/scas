c4fcd8e90a0577c13110ddea9f44c5b898049ea6
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-11 14:03:49 +0100

    empty

--- scas/src/scas/polynomial/PolynomialWithRepr.scala
+++ scas/src/scas/polynomial/PolynomialWithRepr.scala
@@ -6,7 +6,7 @@ import scala.reflect.ClassTag
 import scas.module.ArrayModule
 import PolynomialWithRepr.Element
 
-abstract class PolynomialWithRepr[T : ClassTag, C, M](dimension: Int) extends Polynomial[Element[T], C, M] {
+trait PolynomialWithRepr[T : ClassTag, C, M](dimension: Int) extends Polynomial[Element[T], C, M] {
   given factory: Polynomial[T, C, M] = deferred
   given module: ArrayModule[T] = ArrayModule(factory)(dimension)
   def apply(p: T, n: Int) = (p, module.generator(n))
