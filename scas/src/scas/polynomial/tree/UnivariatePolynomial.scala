b5d5df559c7ebeb69db3809cc14da9f5cde3c956
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-13 13:26:04 +0100

    empty

--- scas/src/scas/polynomial/tree/UnivariatePolynomial.scala
+++ scas/src/scas/polynomial/tree/UnivariatePolynomial.scala
@@ -3,11 +3,15 @@ package scas.polynomial.tree
 import scas.power.{PowerProduct, Lexicographic}
 import scas.structure.commutative.{UniqueFactorizationDomain, Field}
 import scas.variable.Variable
-import scas.util.Conversion
+import scas.util.{Conversion, unary_~}
 import scas.polynomial.TreePolynomial
 import TreePolynomial.Element
 
-class UnivariatePolynomial[C, S : Conversion[Variable]](using val ring: Field[C])(s: S) extends TreePolynomial[C, Array[Int]] with scas.polynomial.ufd.UnivariatePolynomial[Element[C, Array[Int]], C, Array[Int]] with UniqueFactorizationDomain.Conv[Element[C, Array[Int]]] {
+class UnivariatePolynomial[C](using val ring: Field[C])(s: Variable) extends TreePolynomial[C, Array[Int]] with scas.polynomial.ufd.UnivariatePolynomial[Element[C, Array[Int]], C, Array[Int]] with UniqueFactorizationDomain.Conv[Element[C, Array[Int]]] {
   given pp: PowerProduct[Array[Int]] = Lexicographic.inlined(0)(s)
-  given instance: UnivariatePolynomial[C, S] = this
+  given instance: UnivariatePolynomial[C] = this
+}
+
+object UnivariatePolynomial {
+  def apply[C, S : Conversion[Variable]](ring: Field[C])(s: S) = new UnivariatePolynomial(using ring)(~s)
 }
