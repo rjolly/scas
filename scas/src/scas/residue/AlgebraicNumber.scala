b34b13f1372932d7da359b7c171332bfea7a291a
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-13 13:53:25 +0100

    empty

--- scas/src/scas/residue/AlgebraicNumber.scala
+++ scas/src/scas/residue/AlgebraicNumber.scala
@@ -8,7 +8,7 @@ import scas.util.{Conversion, unary_~}
 import scas.variable.Variable
 
 class AlgebraicNumber[C](using var ring: PolynomialOverFieldWithGB[Element[C, Array[Int]], C, Int]) extends ResidueOverField[Element[C, Array[Int]], C, Int] {
-  def this(ring: Field[C])(s: Variable*) = this(using new scas.polynomial.tree.PolynomialOverFieldWithGB(using ring, DegreeReverseLexicographic(0)(s*)))
+  def this(ring: Field[C])(s: Variable*) = this(using new scas.polynomial.tree.PolynomialOverFieldWithGB(using ring, new DegreeReverseLexicographic[Int](s*)))
   def extend(s: Variable*): Unit = {
     ring = ring.newInstance((ring.variables ++ s)*)
     mods = mods.map(_.convert)
