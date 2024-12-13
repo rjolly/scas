77949cf5c23a835c33562302a13eefb730d9af1e
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-13 15:32:29 +0100

    empty

--- scas/src/scas/polynomial/tree/MultivariatePolynomialOverField.scala
+++ scas/src/scas/polynomial/tree/MultivariatePolynomialOverField.scala
@@ -5,6 +5,6 @@ import scas.structure.commutative.{UniqueFactorizationDomain, Field}
 import scas.variable.Variable
 import scas.polynomial.TreePolynomial.Element
 
-class MultivariatePolynomialOverField[C](using val ring: Field[C])(s: Variable*) extends MultivariatePolynomial[C](s*) with scas.polynomial.ufd.MultivariatePolynomialOverField[Element, C, Array[Int]] {
+class MultivariatePolynomialOverField[C](using val ring: Field[C])(val s: Variable*) extends MultivariatePolynomial[C] with scas.polynomial.ufd.MultivariatePolynomialOverField[Element, C, Array[Int]] {
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[Array[Int]]) => new PolynomialWithSubresGCD(using ring)(pp.variables*)
 }
