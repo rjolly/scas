27dd6a005054dfcc1c1610b4b063b9a9b60ce130
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-13 14:35:42 +0100

    empty

--- scas/src/scas/polynomial/repr/UnivariatePolynomial.scala
+++ scas/src/scas/polynomial/repr/UnivariatePolynomial.scala
@@ -6,7 +6,7 @@ import scas.structure.commutative.Field
 import scas.power.PowerProduct
 import PolynomialWithRepr.Element
 
-class UnivariatePolynomial[T : ClassTag, C, M](using val factory: scas.polynomial.ufd.UnivariatePolynomial[T, C, M])(dimension: Int) extends PolynomialWithRepr[T, C, M](dimension) with scas.polynomial.ufd.UnivariatePolynomial[Element[T], C, M] {
+class UnivariatePolynomial[T, C, M](using val factory: scas.polynomial.ufd.UnivariatePolynomial[T, C, M])(val dimension: Int)(using val cm: ClassTag[T]) extends PolynomialWithRepr[T, C, M] with scas.polynomial.ufd.UnivariatePolynomial[Element[T], C, M] {
   given ring: Field[C] = factory.ring
   given pp: PowerProduct[M] = factory.pp
 }
