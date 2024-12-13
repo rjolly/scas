b624489d5bc3c5ce3db38daa6fd3043beaa21d0e
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-13 13:53:25 +0100

    empty

--- scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
@@ -3,9 +3,8 @@ package scas.polynomial.tree
 import scas.power.PowerProduct
 import scas.structure.commutative.UniqueFactorizationDomain
 import scas.variable.Variable
-import scas.util.Conversion
 import scas.polynomial.TreePolynomial.Element
 
-class PolynomialWithPrimitiveGCD[C, S : Conversion[Variable]](using val ring: UniqueFactorizationDomain[C])(s: S*) extends MultivariatePolynomial[C, S](s*) with scas.polynomial.ufd.PolynomialWithPrimitiveGCD[Element, C, Array[Int]] {
+class PolynomialWithPrimitiveGCD[C](using val ring: UniqueFactorizationDomain[C])(s: Variable*) extends MultivariatePolynomial[C](s*) with scas.polynomial.ufd.PolynomialWithPrimitiveGCD[Element, C, Array[Int]] {
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[Array[Int]]) => new PolynomialWithPrimitiveGCD(using ring)(pp.variables*)
 }
--- scas/src/scas/polynomial/tree/PolynomialWithSubresGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithSubresGCD.scala
@@ -3,9 +3,8 @@ package scas.polynomial.tree
 import scas.power.PowerProduct
 import scas.structure.commutative.UniqueFactorizationDomain
 import scas.variable.Variable
-import scas.util.Conversion
 import scas.polynomial.TreePolynomial.Element
 
-class PolynomialWithSubresGCD[C, S : Conversion[Variable]](using val ring: UniqueFactorizationDomain[C])(s: S*) extends MultivariatePolynomial[C, S](s*) with scas.polynomial.ufd.PolynomialWithSubresGCD[Element, C, Array[Int]] {
+class PolynomialWithSubresGCD[C](using val ring: UniqueFactorizationDomain[C])(s: Variable*) extends MultivariatePolynomial[C](s*) with scas.polynomial.ufd.PolynomialWithSubresGCD[Element, C, Array[Int]] {
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[Array[Int]]) => new PolynomialWithSubresGCD(using ring)(pp.variables*)
 }
