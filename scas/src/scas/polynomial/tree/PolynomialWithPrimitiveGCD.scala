7287ce8ee556f71a9bef013ca8f637613d402572
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-13 15:42:36 +0100

    empty

--- scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithPrimitiveGCD.scala
@@ -5,6 +5,6 @@ import scas.structure.commutative.UniqueFactorizationDomain
 import scas.variable.Variable
 import scas.polynomial.TreePolynomial.Element
 
-class PolynomialWithPrimitiveGCD[C](using val ring: UniqueFactorizationDomain[C])(val s: Variable*) extends MultivariatePolynomial[C] with scas.polynomial.ufd.PolynomialWithPrimitiveGCD[Element, C, Array[Int]] {
+class PolynomialWithPrimitiveGCD[C](using UniqueFactorizationDomain[C])(val s: Variable*) extends MultivariatePolynomial[C] with scas.polynomial.ufd.PolynomialWithPrimitiveGCD[Element, C, Array[Int]] {
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[Array[Int]]) => new PolynomialWithPrimitiveGCD(using ring)(pp.variables*)
 }
--- scas/src/scas/polynomial/tree/PolynomialWithSimpleGCD.scala
+++ scas/src/scas/polynomial/tree/PolynomialWithSimpleGCD.scala
@@ -5,6 +5,6 @@ import scas.structure.commutative.UniqueFactorizationDomain
 import scas.variable.Variable
 import scas.polynomial.TreePolynomial.Element
 
-class PolynomialWithSimpleGCD[C](using val ring: UniqueFactorizationDomain[C])(val s: Variable*) extends MultivariatePolynomial[C] with scas.polynomial.ufd.PolynomialWithSimpleGCD[Element, C, Array[Int]] {
+class PolynomialWithSimpleGCD[C](using UniqueFactorizationDomain[C])(val s: Variable*) extends MultivariatePolynomial[C] with scas.polynomial.ufd.PolynomialWithSimpleGCD[Element, C, Array[Int]] {
   def newInstance = [C] => (ring: UniqueFactorizationDomain[C], pp: PowerProduct[Array[Int]]) => new PolynomialWithSimpleGCD(using ring)(pp.variables*)
 }
