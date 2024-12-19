69d0e1107c67d15615e171c09be7837562ac790f
Raphael Jolly <rjolly@users.sourceforge.net>
2024-12-19 17:05:52 +0100

    empty

--- scas/src/scas/structure/VectorSpace.scala
+++ scas/src/scas/structure/VectorSpace.scala
@@ -1,9 +1,10 @@
 package scas.structure
 
+import scala.compiletime.deferred
 import scas.util.{Conversion, unary_~}
 
 trait VectorSpace[T, R] extends Module[T, R] {
-  given ring: Field[R]
+  given ring: Field[R] = deferred
   extension (x: T) def divideRight(y: R) = x%* ring.inverse(y)
   extension (x: T) def %/ [U: Conversion[R]](y: U) = x.divideRight(~y)
 }
