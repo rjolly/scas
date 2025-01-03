9cb755e8c7a4ecc62881c519f574606d1d822219
Raphael Jolly <rjolly@users.sourceforge.net>
2025-01-03 10:54:52 +0100

    empty

--- application/build.js
+++ application/build.js
@@ -1,15 +1,20 @@
 mkdir("build");
 mkdir("build/classes");
+mkdir("build/sources");
 mkdir("build/javadoc");
 
 var name = "scas.application";
 dotc("../" + name.replace(".", "/") + "/src", "build/classes");
 copy("../" + name.replace(".", "/") + "/resources", "build/classes");
+copy("../" + name.replace(".", "/") + "/src", "build/sources");
+copy("../" + name.replace(".", "/") + "/resources", "build/sources");
 dottydoc("build/classes", "build/javadoc");
 
 mkdir("dist");
 var name_rev = name + "_3";
 jar("dist/" + name_rev + ".jar", "build/classes", ".*", "manifest.mf");
+jar("dist/" + name_rev + "-source.jar", "build/sources");
 jar("dist/" + name_rev + "-javadoc.jar", "build/javadoc");
+cp("pom.xml", "dist/" + name_rev + ".pom")
 
 publish("dist")
