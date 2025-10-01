22ef707a2c2635f9eaa0621fd58a948d4973135d
Raphael Jolly <rjolly@users.sourceforge.net>
2025-10-01 15:10:32 +0200

    empty

--- scas/application/src/scas/scripting/Poly.scala
+++ scas/application/src/scas/scripting/Poly.scala
@@ -1,6 +1,7 @@
 package scas.scripting
 
-import scas.polynomial.tree.PolynomialWithSubresGCD
+import scas.power.growable.Lexicographic
+import scas.polynomial.tree.growable.PolynomialWithGB
 import scas.polynomial.TreePolynomial.Element
 import scas.variable.Variable
 import scas.base.BigInteger
@@ -9,5 +10,5 @@ import BigInteger.given
 type Poly = Element[BigInteger, Array[Int]]
 
 object Poly {
-  def apply(variables: Variable*) = PolynomialWithSubresGCD(variables*)
+  def apply(variables: Variable*) = new scas.polynomial.tree.growable.PolynomialWithGB(using BigInteger, new Lexicographic[Int](variables*))
 }
