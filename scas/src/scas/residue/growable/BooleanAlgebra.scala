476760d2c4f9d7ab247be0872834c85906d490b4
Raphael Jolly <rjolly@users.sourceforge.net>
2025-10-03 11:54:14 +0200

    empty

--- scas/src/scas/residue/growable/BooleanAlgebra.scala
+++ scas/src/scas/residue/growable/BooleanAlgebra.scala
@@ -4,8 +4,13 @@ import scas.polynomial.TreePolynomial.Element
 import scas.polynomial.ufd.growable.PolynomialWithGB
 import scas.power.growable.Lexicographic
 import scas.variable.Variable
-import scas.base.Boolean
+import scas.base.{BigInteger, Boolean}
+import BigInteger.given
 
 open class BooleanAlgebra(using PolynomialWithGB[Element[Boolean, Array[Int]], Boolean, Int]) extends Residue[Element[Boolean, Array[Int]], Boolean, Int] with scas.residue.BooleanAlgebra.Impl {
   def this(variables: Variable*) = this(using new scas.polynomial.tree.growable.PolynomialWithGB(using Boolean, new Lexicographic[Int](variables*)))
+  override def extend(variables: Variable*): Unit = {
+    super.extend(variables*)
+    update(generators.drop(ring.pp.variables.length - variables.length).map(x => x+x\2)*)
+  }
 }
