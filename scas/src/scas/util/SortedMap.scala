8265c0ad21f142e855bffd7ef124bbb40621963b
Raphael Jolly <rjolly@users.sourceforge.net>
2025-10-28 14:25:59 +0100

    empty

---
+++ scas/src/scas/util/SortedMap.scala
@@ -0,0 +1,6 @@
+package scas.util
+
+import language.experimental.captureChecking
+import scala.caps.Mutable
+
+trait SortedMap[K, V] extends java.util.SortedMap[K, V] with Mutable
---
+++ scas/src/scas/util/TreeMap.scala
@@ -0,0 +1,3 @@
+package scas.util
+
+class TreeMap[K, V] extends java.util.TreeMap[K, V] with SortedMap[K, V]
