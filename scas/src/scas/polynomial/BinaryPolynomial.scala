41927a81ac270190b39bc9011a5ae42136333f12
Raphael Jolly <rjolly@users.sourceforge.net>
2026-09-24 18:10:07 +0200

    empty

--- scas/src/scas/polynomial/BinaryPolynomial.scala
+++ scas/src/scas/polynomial/BinaryPolynomial.scala
@@ -4,7 +4,7 @@ import scala.compiletime.deferred
 import scas.power.compact.BinaryPowerProduct
 import scas.base.ModInteger
 
-trait BinaryPolynomial[T] extends ConvertablePolynomial[T, Int, Int] {
+trait BinaryPolynomial[T] extends ConvertablePolynomial[T, Int, Int] with MutablePolynomial[T, Int, Array[Int]] {
   override given ring: ModInteger = ModInteger("2")
   given pp: BinaryPowerProduct = deferred
 }
--- scas/src/scas/polynomial/PolynomialWithDefining.scala
+++ scas/src/scas/polynomial/PolynomialWithDefining.scala
@@ -6,22 +6,30 @@ import scas.power.compact.PowerProduct
 import scas.base.{BigInteger, ModInteger}
 import BigInteger.given
 
-class PolynomialWithDefining[T](using factory: BinaryPolynomial[T]) extends Polynomial[Element[T], Int, Array[Int]] {
+class PolynomialWithDefining[T](using factory: BinaryPolynomial[T]) extends MutablePolynomial[Element[T], Int, Array[Int]] {
   override given ring: ModInteger = factory.ring
   override given pp: PowerProduct = factory.pp.defining
   def apply(s: (Array[Int], Int)*) = Right(factory(s*))
   @targetName("fromPolynomial") def apply(p: T) = Right(p)
-  override def normalize(x: Element[T]) = x match {
-    case Right(p) => Right(factory.normalize(p))
-    case Left(d) => Left(d)
+  def unmodifiable(x: Element[T]) = x match {
+    case Right(p) => Right(factory.unmodifiable(p))
+    case Left(_) => x
+  }
+  def modifiable(x: Element[T]) = x match {
+    case Right(p) => Right(factory.modifiable(p))
+    case Left(_) => x
+  }
+  override def normalize(x: Element[T]) = {
+    if (x.isRight) then super.normalize(x)
+    else x
   }
   override def s_polynomial(x: Element[T], y: Element[T]) = x match {
-    case Right(p) => y match {
-      case Right(q) => Right(factory.s_polynomial(p, q))
-      case Left(d) => Right(p * factory.generator(d))
+    case Right(_) => y match {
+      case Right(_) => super.s_polynomial(x, y)
+      case Left(d) => x * generator(d)
     }
     case Left(d) => y match {
-      case Right(q) => Right(factory.generator(d) * q)
+      case Right(_) => generator(d) * y
       case Left(_) => ???
     }
   }
@@ -34,13 +42,13 @@ class PolynomialWithDefining[T](using factory: BinaryPolynomial[T]) extends Poly
       case Right(p) => pp.convert(p.headPowerProduct)(factory.pp)
       case Left(d) => pp.convert(factory.generator(d).headPowerProduct)(factory.pp) \ 2
     }
-    override def reduce(ys: Element[T]*) = x match {
-      case Right(p) => Right(p.reduce(ys.flatMap(_.toSeq)*))
-      case Left(d) => Left(d)
+    override def reduce(ys: Element[T]*) = {
+      if (x.isRight) then super.reduce(x)(ys.filter(_.isRight)*)
+      else x
     }
-    override def reduce(strict: Boolean, tail: Boolean, ys: Element[T]*) = x match {
-      case Right(p) => Right(p.reduce(strict, tail, ys.flatMap(_.toSeq)*))
-      case Left(d) => Left(d)
+    override def reduce(strict: Boolean, tail: Boolean, ys: Element[T]*) = {
+      if (x.isRight) then super.reduce(x)(strict, tail, ys.filter(_.isRight)*)
+      else x
     }
     def add(y: Element[T]) = ???
     def map(f: (Array[Int], Int) => (Array[Int], Int)) = ???
